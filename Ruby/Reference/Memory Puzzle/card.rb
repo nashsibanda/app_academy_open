43a47b8739746b10c545621646787a0931e9b1f6
Nash <nashx90@gmail.com>
2019-08-18 01:39:11 +0900

    Add matched attr, and match function

--- Ruby/Reference/Memory Puzzle/card.rb
+++ Ruby/Reference/Memory Puzzle/card.rb
@@ -1,10 +1,11 @@
 class Card
-  attr_reader :face_value, :facing
+  attr_reader :face_value, :facing, :matched
 
   def initialize(face_value, position)
     @face_value = face_value
     @facing = "down"
     @position = position
+    @matched = false
   end
 
   def to_s
@@ -18,6 +19,11 @@ class Card
     when "down"
       @facing = "down"
     end
+  end
+
+  def match
+    @matched = true
+    @facing = "up"
   end
 
 end
