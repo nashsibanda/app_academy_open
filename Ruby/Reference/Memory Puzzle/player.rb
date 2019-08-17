b0239af9066291f358f2303100c7fee22ce52ea3
Nash <nashx90@gmail.com>
2019-08-18 02:11:46 +0900

    Reverse coordinates for player guess

--- Ruby/Reference/Memory Puzzle/player.rb
+++ Ruby/Reference/Memory Puzzle/player.rb
@@ -2,7 +2,7 @@ class Player
   
   def guess
     puts "Enter a card position separated by a comma, like this: '0,1'"
-    return gets.chomp.split(",").map(&:to_i)
+    return gets.chomp.split(",").map(&:to_i).reverse
   end
 
 end
