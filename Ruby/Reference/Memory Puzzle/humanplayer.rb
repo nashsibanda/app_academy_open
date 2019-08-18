942b55d460e54cafd18dae12cab2032a3f09fc65
Nash <nashx90@gmail.com>
2019-08-18 13:31:23 +0900

    Fixed guess params

--- Ruby/Reference/Memory Puzzle/humanplayer.rb
+++ Ruby/Reference/Memory Puzzle/humanplayer.rb
@@ -9,7 +9,7 @@ class HumanPlayer
     @seen_cards[position] = card
   end
 
-  def guess(board, first_guess)
+  def guess(first_guess, matched_card_positions)
     puts "Enter a card position separated by a comma, like this: '0,1'"
     return gets.chomp.split(",").map(&:to_i)
   end
