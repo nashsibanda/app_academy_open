069965c30e5c0c6e57ddc047237ae49c15f28aff
Nash <nashx90@gmail.com>
2019-08-18 20:25:03 +0900

    Make populate methods, called by one parent method

--- Ruby/Reference/Sudoku/board.rb
+++ Ruby/Reference/Sudoku/board.rb
@@ -1,7 +1,7 @@
 require_relative "./tile.rb"
 
 class Board
-  attr_reader :board_rows
+  attr_reader :board_rows, :board_cols, :board_squares
   
   def initialize(filename)
     @filename = "./puzzles/#{filename}.txt"
@@ -15,6 +15,7 @@ class Board
   def populate_board
     self.populate_board_rows
     self.populate_board_cols
+    self.populate_board_squares
   end
 
   def populate_board_rows
@@ -29,10 +30,26 @@ class Board
   end
 
   def populate_board_cols
-    
+    (0...@board_rows[0].length).each do |c_idx|
+      @board_rows.each_with_index do |row, r_idx|
+        @board_cols[c_idx][r_idx] = @board_rows[r_idx][c_idx]
+      end
+    end
+  end
+
+  def populate_board_squares
+    @board_squares[0] = @board_rows[0][0..2] + @board_rows[1][0..2] + @board_rows[2][0..2]
+    @board_squares[1] = @board_rows[0][3..5] + @board_rows[1][3..5] + @board_rows[2][3..5]
+    @board_squares[2] = @board_rows[0][6..8] + @board_rows[1][6..8] + @board_rows[2][6..8]
+    @board_squares[3] = @board_rows[3][0..2] + @board_rows[4][0..2] + @board_rows[5][0..2]
+    @board_squares[4] = @board_rows[3][3..5] + @board_rows[4][3..5] + @board_rows[5][3..5]
+    @board_squares[5] = @board_rows[3][6..8] + @board_rows[4][6..8] + @board_rows[5][6..8]
+    @board_squares[6] = @board_rows[6][0..2] + @board_rows[7][0..2] + @board_rows[8][0..2]
+    @board_squares[7] = @board_rows[6][3..5] + @board_rows[7][3..5] + @board_rows[8][3..5]
+    @board_squares[8] = @board_rows[6][6..8] + @board_rows[7][6..8] + @board_rows[8][6..8]
   end
 
 end
 
 temp = Board.new("sudoku1")
-p temp.board_rows
+p temp.board_squares
