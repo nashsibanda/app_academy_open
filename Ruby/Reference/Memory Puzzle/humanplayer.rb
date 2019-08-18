06edf3494e0b5d30b7b13c0546b8f5c79d807aa4
Nash <nashx90@gmail.com>
2019-08-18 11:20:36 +0900

    Undid reversal of player guess coords

--- Ruby/Reference/Memory Puzzle/humanplayer.rb
+++ Ruby/Reference/Memory Puzzle/humanplayer.rb
@@ -11,6 +11,6 @@ class HumanPlayer
 
   def guess
     puts "Enter a card position separated by a comma, like this: '0,1'"
-    return gets.chomp.split(",").map(&:to_i).reverse
+    return gets.chomp.split(",").map(&:to_i)
   end
 end
