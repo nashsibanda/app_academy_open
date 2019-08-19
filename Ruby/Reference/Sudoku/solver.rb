7b2de2d4662500577fefa86903eb5b949131a1e0
Nash <nashx90@gmail.com>
2019-08-19 13:44:10 +0900

    Add and display cycle count for solver

--- Ruby/Reference/Sudoku/solver.rb
+++ Ruby/Reference/Sudoku/solver.rb
@@ -3,12 +3,14 @@ class Solver
   def initialize(board)
     @board = board
     @current_tile = board.changeable_tiles[0]
+    @cycles = 0
   end
 
   def solve
     until @board.solved?
       system("clear")
       @board.render
+      puts "Cycles: #{@cycles}"
       if @current_tile.number < 9
         @current_tile.number += 1
         if @board.unique?(@current_tile)
@@ -18,9 +20,11 @@ class Solver
         @current_tile.number = 0
         @current_tile = @board.parent_tiles[@current_tile]
       end
+      @cycles += 1
     end
     system("clear")
     @board.render
+    puts "Cycles: #{@cycles}"
     puts "Solved!"
     return true
   end
