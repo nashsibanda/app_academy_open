17627a8d93e3ba9ba02069daf4e44fcc9d6ca7a5
Nash <nashx90@gmail.com>
2019-09-04 11:03:52 +0900

    Implement PolyTreeNode class, parent= method

--- Ruby/Data Structures/PolyTreeNode/lib/00_tree_node.rb
+++ Ruby/Data Structures/PolyTreeNode/lib/00_tree_node.rb
@@ -1,3 +1,16 @@
 class PolyTreeNode
 
+  attr_reader :parent, :children, :value
+
+  def initialize(value)
+    @value = value
+    @parent
+    @children = []
+  end
+
+  def parent=(node)
+    @parent = node
+    node.children << self unless node == nil || node.children.include?(self)
+  end
+
 end
