4e02b136e12d1cca75f015e9a44a70666ba711c4
Nash <nashx90@gmail.com>
2019-09-09 16:44:10 +0900

    Add children method to show potential states

--- Ruby/Data Structures/TicTacToe/lib/tic_tac_toe_node.rb
+++ Ruby/Data Structures/TicTacToe/lib/tic_tac_toe_node.rb
@@ -2,7 +2,7 @@ require_relative 'tic_tac_toe'
 
 class TicTacToeNode
   attr_reader :board, :next_mover_mark, :prev_move_pos
-  
+
   def initialize(board, next_mover_mark, prev_move_pos = nil)
     @board = board
     @next_mover_mark = next_mover_mark
@@ -18,5 +18,22 @@ class TicTacToeNode
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+    case @next_mover_mark
+    when :x
+      mark = :o
+    when :o
+      mark = :x
+    end
+    children = []
+    @board.rows.each_with_index do |row, r_idx|
+      row.each_with_index do |spot, s_idx|
+        position = [r_idx, s_idx]
+        if @board.empty?(position)
+          new_node = TicTacToeNode.new(Board.new(@board.dup), mark, position)
+          children << new_node
+        end
+      end
+    end
+    return children
   end
 end
