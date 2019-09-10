93957eca3063247ee78b1ea944e4d40b7b2ec271
Nash <nashx90@gmail.com>
2019-09-10 23:24:22 +0900

    Add move method and logic to super computer

--- Ruby/Data Structures/TicTacToe/lib/super_computer_player.rb
+++ Ruby/Data Structures/TicTacToe/lib/super_computer_player.rb
@@ -2,6 +2,16 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    node = TicTacToeNode.new(game.board, mark)
+    possible_moves = node.children
+    possible_moves.each do |child|
+      return child.prev_move_pos if child.winning_node?(mark)
+    end
+    node = possible_moves.find do |child|
+        !child.losing_node?(mark)
+      end
+    return node.prev_move_pos if node
+    raise "No non-losing nodes found!"
   end
 end
 
