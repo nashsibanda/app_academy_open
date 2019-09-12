2780887879e812f33e875df6836d33521ff6738e
Nash <nashx90@gmail.com>
2019-09-12 15:51:27 +0900

    Add init method and place_stones helper

--- Ruby/Object-Oriented Programming/Mancala/lib/board.rb
+++ Ruby/Object-Oriented Programming/Mancala/lib/board.rb
@@ -2,10 +2,17 @@ class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
+    @cups = Array.new(14) { Array.new }
+    place_stones
   end
 
   def place_stones
     # helper method to #initialize every non-store cup with four stones each
+    @cups.each_with_index do |cup, idx|
+      unless idx == 6 || idx == 13
+        4.times { cup << :stone }
+      end
+    end
   end
 
   def valid_move?(start_pos)
