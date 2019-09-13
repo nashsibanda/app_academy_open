8f35cc02d139fe72442a6f2d29468cd3c2cafadd
Nash <nashx90@gmail.com>
2019-09-13 17:18:54 +0900

    Complete phase 3

--- Ruby/Object-Oriented Programming/Error Handling Funtime/super_useful.rb
+++ Ruby/Object-Oriented Programming/Error Handling Funtime/super_useful.rb
@@ -9,11 +9,25 @@ end
 # PHASE 3
 FRUITS = ["apple", "banana", "orange"]
 
+class CoffeeError < StandardError
+  def message
+    "Jeez Louise, that's some excellent Nicaraguan blended bean juice! What a pal. Got a, you know, actual fruit for me too?"
+  end
+end
+
+class FruitError < StandardError
+  def message
+    "This looks like fruit to you, does it? You know what, maybe you and I are just not meant to be friends. I'm done. Bye."
+  end
+end
+
 def reaction(maybe_fruit)
   if FRUITS.include? maybe_fruit
     puts "OMG, thanks so much for the #{maybe_fruit}!"
-  else 
-    raise StandardError 
+  elsif maybe_fruit == "coffee" 
+    raise CoffeeError
+  else
+    raise FruitError 
   end 
 end
 
@@ -21,9 +35,18 @@ def feed_me_a_fruit
   puts "Hello, I am a friendly monster. :)"
 
   puts "Feed me a fruit! (Enter the name of a fruit:)"
-  maybe_fruit = gets.chomp
-  reaction(maybe_fruit) 
-end  
+  begin
+    maybe_fruit = gets.chomp
+    reaction(maybe_fruit)
+  rescue CoffeeError => err
+    puts err.message
+    retry
+  rescue FruitError => err
+    puts err.message
+  end
+end
+
+p feed_me_a_fruit
 
 # PHASE 4
 class BestFriend
