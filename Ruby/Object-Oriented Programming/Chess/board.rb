2ee950cb666380889ae70098594ddc67b7478cf7
Nash <nashx90@gmail.com>
2019-09-17 12:48:13 +0900

    Add bracket methods, populate placeholder pieces

--- Ruby/Object-Oriented Programming/Chess/board.rb
+++ Ruby/Object-Oriented Programming/Chess/board.rb
@@ -1,11 +1,30 @@
 require_relative "piece_kit"
 
 class Board
-  # attr_reader :rows
+  attr_reader :rows
   
   def initialize
-    @rows = Array.new (8) { Array.new (8) {NullPiece.new} }
-    # @sentinel = NullPiece.new
+    @rows = Array.new (8) { Array.new (8) { NullPiece.instance } }
+    @sentinel = NullPiece.instance
+    populate_pieces
+  end
+
+  def [](position)
+    @rows[position[0]][position[1]]
+  end
+
+  def []=(position, value)
+    @rows[position[0]][position[1]] = value
+  end
+
+  private
+
+  def populate_pieces
+    @rows.each_with_index do |row, r_idx|
+      if r_idx.between?(0,1) || r_idx.between?(6,7)
+        row.each_index { |s_idx| @rows[r_idx][s_idx] = Piece.new }
+      end
+    end
   end
 
 end
