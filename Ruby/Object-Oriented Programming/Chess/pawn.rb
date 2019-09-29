c03af26f676049cd552a4f33f21b5cad5f271252
Nash <nashx90@gmail.com>
2019-09-29 22:51:01 +0900

    Add move_dirs method

--- Ruby/Object-Oriented Programming/Chess/pawn.rb
+++ Ruby/Object-Oriented Programming/Chess/pawn.rb
@@ -5,7 +5,7 @@ class Pawn < Piece
   end
 
   def move_dirs
-    
+    move_dirs = forward_steps + side_attacks
   end
 
   private
@@ -20,15 +20,17 @@ class Pawn < Piece
   end
 
   def forward_steps
+    forward_steps = []
+    forward_steps << [(@position[0] + forward_dir), @position[1]]
     if (@color == :white && @position[0] == 1) || (@color == :black && @position[0] == 6)
-      return 2
+      forward_steps << [(@position[0] + forward_dir + forward_dir), @position[1]]
     end
-    return 1
+    return forward_steps
   end
 
   def side_attacks
     side_attacks = []
-    left, right = [(@position[0] + forward_dir), (@position - 1)], [(@position[0] + forward_dir), (@position + 1)]
+    left, right = [(@position[0] + forward_dir), (@position[1] - 1)], [(@position[0] + forward_dir), (@position[1] + 1)]
     side_attacks << left if !@board[left].empty? && @board[left].color != @color
     side_attacks << right if !@board[right].empty? && @board[right].color != @color
     side_attacks
