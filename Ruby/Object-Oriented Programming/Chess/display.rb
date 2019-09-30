10723c694f3718342ad031fc4c79bbe70245d22f
Nash <nashx90@gmail.com>
2019-09-30 22:53:54 +0900

    Add test render loop, properly hover empty spaces

--- Ruby/Object-Oriented Programming/Chess/display.rb
+++ Ruby/Object-Oriented Programming/Chess/display.rb
@@ -12,13 +12,26 @@ class Display
     render_board = @board.clone
     render_board.rows.each_with_index do |row, r_idx|
       row.each_with_index do |piece, p_idx|
-        if [r_idx, p_idx] == @cursor.cursor_pos
-          piece.color = :hovered
+        pos = [r_idx, p_idx]
+        if pos == @cursor.cursor_pos
+          if render_board[pos].class == NullPiece
+            render_board[pos] = "☐".colorize(:green)
+          else
+            piece.color = :hovered
+          end
         end
       end
       puts row.join(" ")
     end
     # @board.rows.each { |row| puts row.join(" ") }
+  end
+
+  def render_loop
+    while true
+      system("clear")
+      render
+      @cursor.get_input
+    end
   end
 
   private
@@ -31,4 +44,4 @@ end
 
 tempboard = Board.new
 tempdisplay = Display.new(tempboard)
-tempdisplay.render
+tempdisplay.render_loop
