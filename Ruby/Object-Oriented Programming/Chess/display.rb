5168deda1e3090cde95f455d0086050007cc44e8
Nash <nashx90@gmail.com>
2019-09-30 16:12:40 +0900

    Add render method, cursor hovered color changes

--- Ruby/Object-Oriented Programming/Chess/display.rb
+++ Ruby/Object-Oriented Programming/Chess/display.rb
@@ -1,4 +1,5 @@
 require_relative "cursor"
+require_relative "board"
 
 class Display
   
@@ -7,4 +8,27 @@ class Display
     @cursor = Cursor.new([0,0], board)
   end
 
+  def render
+    render_board = @board.clone
+    render_board.rows.each_with_index do |row, r_idx|
+      row.each_with_index do |piece, p_idx|
+        if [r_idx, p_idx] == @cursor.cursor_pos
+          piece.color = :hovered
+        end
+      end
+      puts row.join(" ")
+    end
+    # @board.rows.each { |row| puts row.join(" ") }
+  end
+
+  private
+
+  def cursor_color
+    
+  end
+
 end
+
+tempboard = Board.new
+tempdisplay = Display.new(tempboard)
+tempdisplay.render
