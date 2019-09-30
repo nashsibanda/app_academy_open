5a9aa3cf094737b87f2b7f243f30c044cd4caa4e
Nash <nashx90@gmail.com>
2019-09-30 12:14:15 +0900

    Specs all passed, building out methods

--- Ruby/Object-Oriented Programming/Simon/lib/simon.rb
+++ Ruby/Object-Oriented Programming/Simon/lib/simon.rb
@@ -4,31 +4,51 @@ class Simon
   attr_accessor :sequence_length, :game_over, :seq
 
   def initialize
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 
   def play
-
+    take_turn until @game_over
+    if @game_over
+      game_over_message
+      reset_game
+    end
   end
 
   def take_turn
-
+    show_sequence
+    require_sequence
+    if !@game_over
+      round_success_message
+      @sequence_length += 1
+    end
   end
 
   def show_sequence
-
+    add_random_color
+    @seq.each do |color|
+      puts color
+      sleep (0.5)
+      system("clear")
+    end
   end
 
   def require_sequence
-
+    input = []
+    until input.length == @sequence_length
+      input << gets.chomp
+      system("clear")
+    end
   end
 
   def add_random_color
-
+    seq << COLORS.sample
   end
 
   def round_success_message
-
+    puts "Nice work, let's keep going."
   end
 
   def game_over_message
@@ -36,6 +56,6 @@ class Simon
   end
 
   def reset_game
-
+    initialize
   end
 end
