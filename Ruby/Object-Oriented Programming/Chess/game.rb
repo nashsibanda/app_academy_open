0fe654fb1f00d440b99d20ea8bf6a0a498d22f7c
Nash <nashx90@gmail.com>
2019-10-02 01:31:27 +0900

    Make initialize method, require correct files

--- Ruby/Object-Oriented Programming/Chess/game.rb
+++ Ruby/Object-Oriented Programming/Chess/game.rb
@@ -1,11 +1,18 @@
+require_relative "board"
 require_relative "display"
-require_relative "human_player"
+require_relative "player_kit"
 
 class Game
+  attr_reader :display
   
   def initialize
     @board = Board.new
-    @player1, @player2 = HumanPlayer.new(:white, @board), HumanPlayer.new(:black, @board)
+    @display = Display.new(@board)
+    @player1, @player2 = HumanPlayer.new(:white, @display), HumanPlayer.new(:black, @display)
+    @current_player = @player1
   end
 
 end
+
+game = Game.new
+game.display.render_loop
