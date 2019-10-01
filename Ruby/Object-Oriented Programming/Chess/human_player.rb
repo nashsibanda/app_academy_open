c06a793ba1a52ef98595f8714a0a2069794deb15
Nash <nashx90@gmail.com>
2019-10-02 01:30:20 +0900

    Add Player and HumanPlayer, #make_move

--- Ruby/Object-Oriented Programming/Chess/human_player.rb
+++ Ruby/Object-Oriented Programming/Chess/human_player.rb
@@ -1,8 +1,15 @@
-class HumanPlayer
-  
-  def initialize(color, board)
-    @color = color
-    @board = board
+class HumanPlayer < Player
+
+  def make_move(_board)
+    start_position, end_position = nil, nil
+    @display.cursor.get_input until @display.cursor.selected == true
+    start_position = @display.cursor.cursor_pos
+    @display.cursor.get_input until @display.cursor.selected == false
+    end_position = @display.cursor.cursor_pos
+    raise "Piece deselected." if start_position == end_position
+    @display.board.move_piece(@color, start_position, end_position)
+  rescue
+    retry
   end
 
 end
