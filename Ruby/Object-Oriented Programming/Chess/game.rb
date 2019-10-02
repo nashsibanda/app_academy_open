14e8c71fbbf0d46add59e0c6d1c9c8ff03752132
Nash <nashx90@gmail.com>
2019-10-02 11:29:49 +0900

    Add game loop, exceptions and messages

--- Ruby/Object-Oriented Programming/Chess/game.rb
+++ Ruby/Object-Oriented Programming/Chess/game.rb
@@ -12,7 +12,38 @@ class Game
     @current_player = @player1
   end
 
+  def play
+    until checkmate?
+      begin
+        @current_player.make_move(@board)
+        switch_player
+      rescue StandardError => e
+        puts e.message
+        sleep(2)
+      end
+    end
+    @display.render
+    switch_player
+    puts "#{@current_player.color.to_s.capitalize} wins!"
+  end
+
+  private
+
+  def switch_player
+    case @current_player
+    when @player1
+      @current_player = @player2
+    when @player2
+      @current_player = @player1
+    end
+  end
+
+  def checkmate?
+    return true if @board.checkmate?(@player1.color) || @board.checkmate?(@player2.color)
+    false
+  end
+
 end
 
 game = Game.new
-game.display.render_loop
+game.play
--- Ruby/Object-Oriented Programming/Chess/human_player.rb
+++ Ruby/Object-Oriented Programming/Chess/human_player.rb
@@ -1,15 +1,26 @@
 class HumanPlayer < Player
 
-  def make_move(_board)
+  def make_move(board)
     start_position, end_position = nil, nil
-    @display.cursor.get_input until @display.cursor.selected == true
+    until @display.cursor.selected == true
+      @display.render
+      puts "#{@color.to_s.capitalize} player's turn."
+      @display.cursor.get_input
+    end
     start_position = @display.cursor.cursor_pos
-    @display.cursor.get_input until @display.cursor.selected == false
+    puts "Start position: #{start_position}"
+    until @display.cursor.selected == false
+      @display.render
+      puts "#{@color.to_s.capitalize} player's turn."
+      @display.cursor.get_input
+    end
     end_position = @display.cursor.cursor_pos
+    puts "End position: #{end_position}"
     raise "Piece deselected." if start_position == end_position
-    @display.board.move_piece(@color, start_position, end_position)
   rescue
     retry
+  else
+    board.move_piece(@color, start_position, end_position)
   end
 
 end
