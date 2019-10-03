291bafe614519ad04504f2cefa4be7b5b6bcf6b9
Nash <nashx90@gmail.com>
2019-10-03 09:56:42 +0900

    Refactor #make_move

--- Ruby/Object-Oriented Programming/Chess/human_player.rb
+++ Ruby/Object-Oriented Programming/Chess/human_player.rb
@@ -2,18 +2,10 @@ class HumanPlayer < Player
 
   def make_move(board)
     start_position, end_position = nil, nil
-    until @display.cursor.selected == true
-      @display.render
-      puts "#{@color.to_s.capitalize} player's turn."
-      @display.cursor.get_input
-    end
+    turn_loop until @display.cursor.selected == true
     start_position = @display.cursor.cursor_pos
     puts "Start position: #{start_position}"
-    until @display.cursor.selected == false
-      @display.render
-      puts "#{@color.to_s.capitalize} player's turn."
-      @display.cursor.get_input
-    end
+    turn_loop until @display.cursor.selected == false
     end_position = @display.cursor.cursor_pos
     puts "End position: #{end_position}"
     raise "Piece deselected." if start_position == end_position
@@ -21,6 +13,14 @@ class HumanPlayer < Player
     retry
   else
     board.move_piece(@color, start_position, end_position)
+  end
+
+  private
+
+  def turn_loop
+    @display.render
+    puts "#{@color.to_s.capitalize} player's turn."
+    @display.cursor.get_input
   end
 
 end
