7b35ea7b620c34288d7d6c332e3b281660fff3eb
Nash <nashx90@gmail.com>
2019-10-10 10:58:53 +0900

    Add #fast_dance, for O(1)

--- Ruby/Big-O/Octopus Problems/dancing_octopus.rb
+++ Ruby/Big-O/Octopus Problems/dancing_octopus.rb
@@ -1,11 +1,36 @@
-tiles_array = ["up", "right-up", "right", "right-down", "down", "left-down", "left",  "left-up" ]
+TILES_ARRAY = ["up", "right-up", "right", "right-down", "down", "left-down", "left",  "left-up" ]
+tiles_hash = {
+  "up" => 0, 
+  "right-up" => 1, 
+  "right" => 2, 
+  "right-down" => 3,
+  "down" => 4, 
+  "left-down" => 5, 
+  "left" => 6, 
+   "left-up" => 7
+}
+
+# def make_hash
+#   tiles_array.each_with_index { |tile, idx| tiles_hash[tile] = idx }
+# end
 
 def slow_dance(direction, tiles)
   tiles.each_with_index { |tile, idx| return idx if direction == tile }
 end
 
-p slow_dance("up", tiles_array)
+def fast_dance(direction, tiles)
+  return tiles[direction]
+end
+
+p slow_dance("up", TILES_ARRAY)
 #> 0
 
-p slow_dance("right-down", tiles_array)
+p slow_dance("right-down", TILES_ARRAY)
+#> 3
+puts
+# make_hash
+p fast_dance("up", tiles_hash)
+#> 0
+
+p fast_dance("right-down", tiles_hash)
 #> 3
