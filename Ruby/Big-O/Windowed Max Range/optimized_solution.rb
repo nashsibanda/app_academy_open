fb015bf4766af24800d92c7485e13a109b3e22bb
Nash <nashx90@gmail.com>
2019-10-13 16:42:17 +0900

    Begin optimized solution, add ::MyQueue w/ methods

---
+++ Ruby/Big-O/Windowed Max Range/optimized_solution.rb
@@ -0,0 +1,25 @@
+class MyQueue
+  def initialize
+    @store = []
+  end
+
+  def peek
+    @store[0]
+  end
+
+  def size
+    @store.size
+  end
+
+  def empty?
+    @store.empty?
+  end
+
+  def enqueue(element)
+    @store.push(element)
+  end
+
+  def dequeue
+    @store.shift
+  end
+end
