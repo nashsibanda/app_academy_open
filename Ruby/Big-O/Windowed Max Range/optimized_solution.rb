86c264454adfdd136ff5c57cd2364bee6973004c
Nash <nashx90@gmail.com>
2019-10-13 16:46:42 +0900

    Add ::MyStack with methods

--- Ruby/Big-O/Windowed Max Range/optimized_solution.rb
+++ Ruby/Big-O/Windowed Max Range/optimized_solution.rb
@@ -14,12 +14,39 @@ class MyQueue
   def empty?
     @store.empty?
   end
-
+  
   def enqueue(element)
     @store.push(element)
   end
-
+  
   def dequeue
     @store.shift
   end
+end
+
+class MyStack
+  def initialize
+    @store = []
+  end
+  
+  def peek
+    @store[0]
+  end
+  
+  def size
+    @store.size
+  end
+  
+  def empty?
+    @store.empty?
+  end
+
+  def pop
+    @store.pop
+  end
+
+  def push
+    @store.push
+  end
+  
 end
