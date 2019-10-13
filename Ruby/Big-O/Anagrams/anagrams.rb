ba5a4b69558dbbd3c7d64676007ce94d73c96fad
Nash <nashx90@gmail.com>
2019-10-13 14:01:59 +0900

    Add #second_anagram? for time complexity O(n^2)

--- Ruby/Big-O/Anagrams/anagrams.rb
+++ Ruby/Big-O/Anagrams/anagrams.rb
@@ -3,6 +3,15 @@ def first_anagram?(string1, string2)
   return anagrams.include?(string2)
 end
 
-puts "#first_anagram?"
-p first_anagram?("gizmod", "sallys")    #=> false
-p first_anagram?("elvis", "lives")    #=> true
+def second_anagram?(string1, string2)
+  string1.each_char { |char| string2.delete!(char) }
+  string2.empty?
+end
+
+# puts "#first_anagram?"
+# p first_anagram?("gizmo", "sally")    #=> false
+# p first_anagram?("elvis", "lives")    #=> true
+
+puts "#second_anagram?"
+p second_anagram?("gizmo", "sally")    #=> false
+p second_anagram?("elvis", "lives")    #=> true
