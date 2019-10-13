860ea65f99b2f8dfbadfa67dd0ae620939b67b7e
Nash <nashx90@gmail.com>
2019-10-13 13:18:00 +0900

    Complete contiguous sub_sum exercise

--- Ruby/Big-O/Execution Time Differences - Algorithms/largest_continuous_sub-sum.rb
+++ Ruby/Big-O/Execution Time Differences - Algorithms/largest_continuous_sub-sum.rb
@@ -13,6 +13,21 @@ def largest_contiguous_subsum(list)
   sub_arrays.map!(&:sum).max
 end
 
+def efficient_subsum(list)
+  debugger
+  largest = list.first
+  current = list.first
+
+  return list.max if list.all? { |num| num < 0 }
+
+  list.drop(1).each do |num|
+    current = 0 if current < 0
+    current += num
+    largest = current if current > largest
+  end
+  largest
+end
+
 p largest_contiguous_subsum(list1) # => 8 (from [7, -6, 7])
 # possible sub-sums
 # [5]           # => 5
@@ -23,7 +38,7 @@ p largest_contiguous_subsum(list1) # => 8 (from [7, -6, 7])
 # [-7]          # => -7
 
 list2 = [2, 3, -6, 7, -6, 7]
-p largest_contiguous_subsum(list2) # => 8
+p efficient_subsum(list1) # => 8
 
 list3 = [-5, -1, -3]
 p largest_contiguous_subsum(list3) # => -1 (from [-1])
