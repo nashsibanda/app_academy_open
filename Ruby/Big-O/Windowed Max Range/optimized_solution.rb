ea7e060e29ce02ba4df62ad9532fa4de8d9c9617
Nash <nashx90@gmail.com>
2019-10-15 10:38:10 +0900

    Complete optimized solution, debugged

--- Ruby/Big-O/Windowed Max Range/optimized_solution.rb
+++ Ruby/Big-O/Windowed Max Range/optimized_solution.rb
@@ -1,3 +1,5 @@
+require "byebug"
+
 class MyQueue
   def initialize
     @store = []
@@ -61,7 +63,7 @@ class StackQueue
   end
 
   def empty?
-    @active_stack.empty?
+    @stack1.empty? && @stack2.empty?
   end
 
   def enqueue(element)
@@ -76,7 +78,7 @@ class StackQueue
   private
 
   def stack_flipper
-    until @stack2.empty?
+    until @stack1.empty?
       @stack2.push(@stack1.pop)
     end
   end
@@ -88,7 +90,7 @@ class MinMaxStack
   def initialize
     @store = MyStack.new
   end
-
+  
   def max
     @store.peek[:max] if !@store.empty?
   end
@@ -108,24 +110,89 @@ class MinMaxStack
   def empty?
     @store.empty?
   end
-
+  
   def pop
     @store.pop[:value] if !@store.empty?
   end
-
+  
   def push(element)
     @store.push( {max: calc_max(element), min: calc_min(element), value: element } )
   end
-
+  
   private
-
+  
   def calc_max(element)
     return element if empty?
     [max, element].max
   end
   
-  def calc_max(element)
+  def calc_min(element)
     return element if empty?
     [min, element].min
   end
 end
+
+class MinMaxStackQueue
+  def initialize
+    @stack1, @stack2 = MinMaxStack.new, MinMaxStack.new
+  end
+
+  def size
+    @stack1.size + @stack2.size
+  end
+
+  def empty?
+    @stack1.empty? && @stack2.empty?
+  end
+
+  def enqueue(element)
+    @stack1.push(element)
+  end
+
+  def dequeue
+    stack_flipper if @stack2.empty?
+    @stack2.pop
+  end
+
+  def max
+    max_options = []
+    max_options << @stack1.max unless @stack1.empty?
+    max_options << @stack2.max unless @stack2.empty?
+    max_options.max
+  end
+
+  def min
+    min_options = []
+    min_options << @stack1.min unless @stack1.empty?
+    min_options << @stack2.min unless @stack2.empty?
+    min_options.min
+  end
+
+  private
+
+  def stack_flipper
+    until @stack1.empty?
+      @stack2.push(@stack1.pop)
+    end
+  end
+end
+
+def windowed_max_range(array, window_size)
+  # debugger
+  queue = MinMaxStackQueue.new
+  current_max_range = 0
+  array.each do |num|
+    queue.enqueue(num)
+    queue.dequeue if queue.size > window_size
+    if queue.size == window_size
+      range = queue.max - queue.min
+      current_max_range = range if range > current_max_range
+    end
+  end
+  return current_max_range
+end
+
+p windowed_max_range([1, 0, 2, 5, 4, 8], 2) == 4 # 4, 8
+p windowed_max_range([1, 0, 2, 5, 4, 8], 3) == 5 # 0, 2, 5
+p windowed_max_range([1, 0, 2, 5, 4, 8], 4) == 6 # 2, 5, 4, 8
+p windowed_max_range([1, 3, 2, 5, 4, 8], 5) == 6 # 3, 2, 5, 4, 8
