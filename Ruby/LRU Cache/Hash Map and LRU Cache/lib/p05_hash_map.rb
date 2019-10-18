2040f6a5c4121f65f040929617ad08cd1750bcdd
Nash <nashx90@gmail.com>
2019-10-19 02:44:02 +0900

    Complete HashMap, add #each and #resize!

--- Ruby/LRU Cache/Hash Map and LRU Cache/lib/p05_hash_map.rb
+++ Ruby/LRU Cache/Hash Map and LRU Cache/lib/p05_hash_map.rb
@@ -1,6 +1,7 @@
 require_relative 'p04_linked_list'
 
 class HashMap
+  include Enumerable
   attr_accessor :count
 
   def initialize(num_buckets = 8)
@@ -17,6 +18,7 @@ class HashMap
       bucket(key).update(key, val)
     else
       @count += 1
+      resize! if @count > num_buckets
       bucket(key).append(key, val)
     end
   end
@@ -32,16 +34,24 @@ class HashMap
     end
   end
 
-  def each
+  def each(&block)
+    node_array = []
+    @store.each do |list|
+      list.each do |node|
+        node_array << node
+      end
+    end
+    node_array.map! { |node| [node.key, node.val] }
+    node_array.each(&block)
   end
 
   # uncomment when you have Enumerable included
-  # def to_s
-  #   pairs = inject([]) do |strs, (k, v)|
-  #     strs << "#{k.to_s} => #{v.to_s}"
-  #   end
-  #   "{\n" + pairs.join(",\n") + "\n}"
-  # end
+  def to_s
+    pairs = inject([]) do |strs, (k, v)|
+      strs << "#{k.to_s} => #{v.to_s}"
+    end
+    "{\n" + pairs.join(",\n") + "\n}"
+  end
 
   alias_method :[], :get
   alias_method :[]=, :set
@@ -53,6 +63,13 @@ class HashMap
   end
 
   def resize!
+    num_buckets.times { @store.push(LinkedList.new) }
+    current_stored = []
+    self.each do |node|
+      key, val = node[0], node[1]
+      delete(key)
+      set(key, val)
+    end
   end
 
   def bucket(key)
