7da2bff35043cda9db524069c1f68457aed94a5b
Nash <nashx90@gmail.com>
2019-10-18 23:25:26 +0900

    Add simple hash func's for Array, String and Hash

--- Ruby/LRU Cache/Hash Map and LRU Cache/lib/p02_hashing.rb
+++ Ruby/LRU Cache/Hash Map and LRU Cache/lib/p02_hashing.rb
@@ -4,11 +4,17 @@ end
 
 class Array
   def hash
+    int_to_hash = self.length
+    self.each_with_index { |el, idx| int_to_hash += (el.object_id * (idx + 1 + self.length)) }
+    int_to_hash.hash
   end
 end
 
 class String
   def hash
+    byte_arr = []
+    self.each_byte { |char| byte_arr << char }
+    byte_arr.hash
   end
 end
 
@@ -16,6 +22,9 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    hash_str_arr = self.to_a.flatten.map(&:to_s)
+    str_hashes_arr = []
+    hash_str_arr.each { |string| str_hashes_arr << string.hash }
+    str_hashes_arr.sort.hash
   end
 end
