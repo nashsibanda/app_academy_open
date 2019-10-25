a568cf58f3da64035f3ff5983624b44b5c8f7501
Nash <nashx90@gmail.com>
2019-10-25 11:15:57 +0900

    Create Playwright class, add ::all and ::find_by_playwright

--- SQL/Object Relational Model/Plays-Playwrights ORM/plays.rb
+++ SQL/Object Relational Model/Plays-Playwrights ORM/plays.rb
@@ -75,3 +75,31 @@ class Play
     SQL
   end
 end
+
+class Playwright
+  attr_accessor
+
+  def self.all
+    data = PlayDBConnection.instance.execute("SELECT * FROM playwrights")
+    data.map { |datum| Playwright.new(datum) }
+  end
+
+  def self.find_by_name(name)
+    data = PlayDBConnection.instance.execute(<<-SQL, name)
+      SELECT
+        *
+      FROM
+        playwrights
+      WHERE
+        name = ?
+    SQL
+    data.map { |datum| Playwright.new(datum) }
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @name = options['name']
+    @birth_year = options['birth_year']
+  end
+
+end
