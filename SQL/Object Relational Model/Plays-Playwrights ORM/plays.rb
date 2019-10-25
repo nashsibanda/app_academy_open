11c634773755ced0bdf86bc8748c828dde6e74a4
Nash <nashx90@gmail.com>
2019-10-25 11:28:49 +0900

    Add Playwright::create, ::update, ::get_plays

--- SQL/Object Relational Model/Plays-Playwrights ORM/plays.rb
+++ SQL/Object Relational Model/Plays-Playwrights ORM/plays.rb
@@ -77,7 +77,7 @@ class Play
 end
 
 class Playwright
-  attr_accessor
+  attr_accessor :id, :name, :birth_year
 
   def self.all
     data = PlayDBConnection.instance.execute("SELECT * FROM playwrights")
@@ -100,6 +100,43 @@ class Playwright
     @id = options['id']
     @name = options['name']
     @birth_year = options['birth_year']
+  end
+
+  def create
+    raise "#{self} already in database" if self.id
+    PlayDBConnection.instance.execute(<<-SQL, self.name, self.birth_year)
+      INSERT INTO
+        playwrights (name, birth_year)
+      VALUES
+        (?, ?)
+    SQL
+    self.id = PlayDBConnection.instance.last_insert_row_id
+  end
+
+  def update
+    raise "#{self} not in database" unless self.id
+    PlayDBConnection.instance.execute(<<-SQL, self.name, self.birth_year, self.id)
+      UPDATE
+        playwrights
+      SET
+        name = ?, birth_year = ?
+      WHERE
+        id = ?
+    SQL
+  end
+
+  def get_plays
+    raise "#{self} not in database" unless self.id
+    PlayDBConnection.instance.execute(<<-SQL, self.id)
+      SELECT
+        *
+      FROM
+        plays
+      JOIN
+        playwrights ON playwrights.id = plays.playwright_id
+      WHERE
+        playwrights.id = ?
+    SQL
   end
 
 end
