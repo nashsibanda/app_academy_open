247b632fe49542da6a9376beee8ea61c27688e4a
Nash <nashx90@gmail.com>
2019-10-28 00:25:59 +0900

    Add User class, working database connection

---
+++ SQL/AA Questions/lib/user.rb
@@ -0,0 +1,16 @@
+require_relative 'reqs'
+
+class User
+  
+
+  def self.all
+    data = QuestionsDatabase.instance.execute("SELECT * FROM users")
+    data.map { |datum| User.new(datum) }
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @fname = options['fname']
+    @lname = options['lname']
+  end
+end
