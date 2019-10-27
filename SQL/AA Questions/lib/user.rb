fe454dffa209043d64844eb91e6b3b43168f8f3e
Nash <nashx90@gmail.com>
2019-10-28 00:36:31 +0900

    Add ::find_by_id and ::find_by_name to User class

--- SQL/AA Questions/lib/user.rb
+++ SQL/AA Questions/lib/user.rb
@@ -1,16 +1,42 @@
 require_relative 'reqs'
 
 class User
-  
-
-  def self.all
-    data = QuestionsDatabase.instance.execute("SELECT * FROM users")
-    data.map { |datum| User.new(datum) }
-  end
+  attr_accessor :id, :fname, :lname
 
   def initialize(options)
     @id = options['id']
     @fname = options['fname']
     @lname = options['lname']
+  end
+
+  # Class Methods
+  def self.all
+    data = QuestionsDatabase.instance.execute("SELECT * FROM users")
+    data.map { |datum| User.new(datum) }
+  end
+  
+  def self.find_by_id(id)
+    data = QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT
+        *
+      FROM
+        users
+      WHERE
+        id = ?
+    SQL
+    data.map { |datum| User.new(datum) }.first
+  end
+
+  def self.find_by_name(fname, lname)
+    data = QuestionsDatabase.instance.execute(<<-SQL, fname, lname)
+      SELECT
+        *
+      FROM
+        users
+      WHERE
+        fname = ? AND
+        lname = ?
+    SQL
+    data.map { |datum| User.new(datum) }
   end
 end
