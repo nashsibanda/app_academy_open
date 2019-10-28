36038a280a86255aacd8c01db1fb1efbc6935fe3
Nash <nashx90@gmail.com>
2019-10-28 15:50:19 +0900

    Add #author, #question, #parent_reply, #child_replies

--- SQL/AA Questions/lib/reply.rb
+++ SQL/AA Questions/lib/reply.rb
@@ -53,5 +53,34 @@ class Reply
     data.map { |datum| Reply.new(datum) }
   end
   
+  def self.find_by_parent_reply_id(parent_reply_id)
+    data = QuestionsDatabase.instance.execute(<<-SQL, parent_reply_id)
+      SELECT
+        *
+      FROM
+        replies
+      WHERE
+        parent_reply_id = ?
+    SQL
+    data.map { |datum| Reply.new(datum) }
+  end
+  
+  # Instance methods
+  def author
+    User.find_by_id(@replier_id)
+  end
+
+  def question
+    Question.find_by_id(@question_id)
+  end
+
+  def parent_reply
+    raise "This is a top-level reply" unless self.parent_reply_id
+    Reply.find_by_id(@parent_reply_id)
+  end
+
+  def child_replies
+    Reply.find_by_parent_reply_id(@id)
+  end
 
 end
