c1ee801aa1a8b97b7658c5938f59ca293b17a10f
Nash <nashx90@gmail.com>
2019-11-05 16:06:42 +0900

    Add #popular_links method for topics

--- SQL/Active Record/URLShortener/app/models/tag_topic.rb
+++ SQL/Active Record/URLShortener/app/models/tag_topic.rb
@@ -5,4 +5,16 @@ class TagTopic < ApplicationRecord
     raise "This topic already exists!" if TagTopic.exists?(topic: topic)
     TagTopic.create!(topic: topic)
   end
+
+  def popular_links
+    links = self.shortened_urls.left_joins(:visits).group(:id).order('COUNT(visits.id) DESC').limit(5)
+    links.map { |link| [link.long_url, "Visited #{link.num_clicks} times"] }
+  end
+
+  has_many :taggings,
+    primary_key: :id,
+    foreign_key: :topic_id,
+    class_name: :Tagging
+  
+  has_many :shortened_urls, through: :taggings, source: :shortened_url
 end
