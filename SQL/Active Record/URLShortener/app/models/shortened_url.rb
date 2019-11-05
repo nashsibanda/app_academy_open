ea111dfab7275529869b323dea00420009b60656
Nash <nashx90@gmail.com>
2019-11-05 16:06:13 +0900

    Add associations for taggings

--- SQL/Active Record/URLShortener/app/models/shortened_url.rb
+++ SQL/Active Record/URLShortener/app/models/shortened_url.rb
@@ -37,6 +37,13 @@ class ShortenedUrl < ApplicationRecord
     primary_key: :id,
     foreign_key: :shortened_url_id,
     class_name: :Visit
+
+  has_many :taggings,
+    primary_key: :id,
+    foreign_key: :shortened_url_id,
+    class_name: :Tagging
+  
+  has_many :tags, through: :taggings, source: :topic
   
   has_many :visitors, Proc.new { distinct }, through: :visits, source: :visitor
 end
--- SQL/Active Record/URLShortener/app/models/tagging.rb
+++ SQL/Active Record/URLShortener/app/models/tagging.rb
@@ -8,4 +8,14 @@ class Tagging < ApplicationRecord
     raise "This link already has the #{topic} tag!" if Tagging.exists?(shortened_url_id: shortened_url.id, topic_id: selected_topic.id)
     Tagging.create!(shortened_url_id: shortened_url.id, topic_id: selected_topic.id)
   end
+
+  belongs_to :shortened_url,
+    primary_key: :id,
+    foreign_key: :shortened_url_id,
+    class_name: :ShortenedUrl
+  
+  belongs_to :topic,
+    primary_key: :id,
+    foreign_key: :topic_id,
+    class_name: :TagTopic
 end
