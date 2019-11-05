9c1aacbeae4eca17f6f34ef29e73f1b78d3b9534
Nash <nashx90@gmail.com>
2019-11-05 10:10:39 +0900

    Add visits through associations, clicks counts for URLs

--- SQL/Active Record/URLShortener/app/models/shortened_url.rb
+++ SQL/Active Record/URLShortener/app/models/shortened_url.rb
@@ -15,10 +15,28 @@ class ShortenedUrl < ApplicationRecord
     ShortenedUrl.create!(long_url: orig_url, short_url: ShortenedUrl.random_code, submitter_id: user.id)
   end
 
+  def num_clicks
+    self.visits.count
+  end
+
+  def num_uniques
+    self.visitors.count
+  end
+
+  def num_recent_uniques
+    recent_uniques = self.visitors.where("visits.created_at >= ?", 10.minutes.ago )
+    recent_uniques.count
+  end
+
   belongs_to :submitter,
     primary_key: :id,
     foreign_key: :submitter_id,
     class_name: :User
   
-  has_many :visitors, through: :visits, source: :visitor
+  has_many :visits,
+    primary_key: :id,
+    foreign_key: :shortened_url_id,
+    class_name: :Visit
+  
+  has_many :visitors, Proc.new { distinct }, through: :visits, source: :visitor
 end
--- SQL/Active Record/URLShortener/app/models/user.rb
+++ SQL/Active Record/URLShortener/app/models/user.rb
@@ -5,6 +5,11 @@ class User < ApplicationRecord
     primary_key: :id,
     foreign_key: :submitter_id,
     class_name: :ShortenedUrl
+
+  has_many :visits,
+    primary_key: :id,
+    foreign_key: :user_id,
+    class_name: :Visit
   
-  has_many :visited_urls, through: :visits, source: :visited_url
+  has_many :visited_urls, Proc.new { distinct }, through: :visits, source: :visited_url
 end
