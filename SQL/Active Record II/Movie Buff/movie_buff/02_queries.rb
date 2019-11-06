cbd21e6a18f6de2e1c92dbcaf27abbeffeb91dea
Nash <nashx90@gmail.com>
2019-11-06 23:13:18 +0900

    Add #eighties_b_movies and #bad_years

--- SQL/Active Record II/Movie Buff/movie_buff/02_queries.rb
+++ SQL/Active Record II/Movie Buff/movie_buff/02_queries.rb
@@ -2,12 +2,24 @@ def eighties_b_movies
   # List all the movies from 1980-1989 with scores falling between
   # 3 and 5 (inclusive).
   # Show the id, title, year, and score.
-
+  Movie
+    .select(:id, :title, :yr, :score)
+    .where(yr: 1980..1989)
+    .where(score: 3..5)
 end
 
 def bad_years
   # List the years in which a movie with a rating above 8 was not released.
-
+  Movie
+    .select(:yr)
+    .where.not(
+      yr: Movie
+            .select(:yr)
+            .where('score > ?', 8)
+            .distinct
+    )
+    .distinct
+    .pluck(:yr)
 end
 
 def cast_list(title)
