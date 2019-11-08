17b49cc466b8359b88f11e152b60ce23d5a719ab
Nash <nashx90@gmail.com>
2019-11-08 11:15:43 +0900

    Validate responses to avoid authors answering own polls

--- SQL/Active Record II/polls_app/app/models/response.rb
+++ SQL/Active Record II/polls_app/app/models/response.rb
@@ -2,6 +2,15 @@ class Response < ApplicationRecord
   validates :user_id, presence: true
   validates :answer_choice_id, presence: true
   validate :not_duplicate_response
+  validate :not_answering_own_poll
+  
+  private
+
+  def not_answering_own_poll
+    if self.user_id == question.poll.author_id
+      errors[:user_id] << 'can not respond to their own poll.'
+    end
+  end
 
   def not_duplicate_response
     if respondent_already_answered?
@@ -15,8 +24,10 @@ class Response < ApplicationRecord
   end
 
   def respondent_already_answered?
-    !self.sibling_responses.empty?
+    sibling_responses.exists?( user_id: self.user_id )
   end
+
+
 
   belongs_to :answer_choice,
     primary_key: :id,
