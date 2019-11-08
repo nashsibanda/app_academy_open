60927f7dcde0f86e3989c4133c808173f601222c
Nash <nashx90@gmail.com>
2019-11-08 16:25:19 +0900

    Add User#completed_polls_sql using pure SQL

--- SQL/Active Record II/polls_app/app/models/user.rb
+++ SQL/Active Record II/polls_app/app/models/user.rb
@@ -1,6 +1,22 @@
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
 
+  def completed_polls_sql
+    Poll.find_by_sql ["SELECT polls.* 
+FROM   polls 
+       JOIN questions 
+         ON questions.poll_id = polls.id 
+       LEFT JOIN (SELECT responses.user_id, 
+                         answer_choices.question_id AS question_id 
+                  FROM   responses 
+                         JOIN answer_choices 
+                           ON answer_choices.id = responses.answer_choice_id 
+                  WHERE  responses.user_id = ?) AS user_responses 
+              ON user_responses.question_id = questions.id 
+GROUP  BY polls.id 
+HAVING Count(user_responses.*) = Count(questions.*)", self.id]
+  end
+
   has_many :authored_polls,
     primary_key: :id,
     foreign_key: :author_id,
