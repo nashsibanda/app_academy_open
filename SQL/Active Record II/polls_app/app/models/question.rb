81185f5dca6353410277d4baeb21d32c3cd11663
Nash <nashx90@gmail.com>
2019-11-08 11:48:12 +0900

    Add Question#results with hash

--- SQL/Active Record II/polls_app/app/models/question.rb
+++ SQL/Active Record II/polls_app/app/models/question.rb
@@ -2,6 +2,18 @@ class Question < ApplicationRecord
   validates :poll_id, presence: true
   validates :text, presence: true
 
+  def results
+    results = {}
+    AnswerChoice
+      .select('answer_choices.text as answer, COUNT(responses.*) as responses')
+      .left_joins(:responses)
+      .where('answer_choices.question_id = ?', self.id)
+      .group('answer')
+      .order('responses DESC')
+      .each { |r| results[r.attributes["answer"]] = r.attributes["responses"] }
+    results
+  end
+
   has_many :answer_choices,
     primary_key: :id,
     foreign_key: :question_id,
