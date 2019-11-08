5052f550b37e05d36993b8bcef856d913ecb6cf8
Nash <nashx90@gmail.com>
2019-11-08 11:00:35 +0900

    Validate responses to avoid multiple answers from 1 user

--- SQL/Active Record II/polls_app/app/models/response.rb
+++ SQL/Active Record II/polls_app/app/models/response.rb
@@ -1,6 +1,22 @@
 class Response < ApplicationRecord
   validates :user_id, presence: true
   validates :answer_choice_id, presence: true
+  validate :not_duplicate_response
+
+  def not_duplicate_response
+    if respondent_already_answered?
+      errors[:user_id] << 'can not respond to the same question multiple times.'
+    end
+  end
+  
+  def sibling_responses
+    self.question.responses
+      .where.not(id: self.id)
+  end
+
+  def respondent_already_answered?
+    !self.sibling_responses.empty?
+  end
 
   belongs_to :answer_choice,
     primary_key: :id,
@@ -11,4 +27,6 @@ class Response < ApplicationRecord
     primary_key: :id,
     foreign_key: :user_id,
     class_name: :User
+  
+  has_one :question, through: :answer_choice
 end
