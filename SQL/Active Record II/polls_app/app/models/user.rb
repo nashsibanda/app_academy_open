b1328c924802586a8b024c7a4218666c505e462e
Nash <nashx90@gmail.com>
2019-11-09 18:32:11 +0900

    Add completed_polls functions, AR method broken

--- SQL/Active Record II/polls_app/app/models/user.rb
+++ SQL/Active Record II/polls_app/app/models/user.rb
@@ -2,19 +2,47 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
 
   def completed_polls_sql
-    Poll.find_by_sql ["SELECT polls.* 
-FROM   polls 
-       JOIN questions 
-         ON questions.poll_id = polls.id 
-       LEFT JOIN (SELECT responses.user_id, 
-                         answer_choices.question_id AS question_id 
-                  FROM   responses 
-                         JOIN answer_choices 
-                           ON answer_choices.id = responses.answer_choice_id 
-                  WHERE  responses.user_id = ?) AS user_responses 
-              ON user_responses.question_id = questions.id 
-GROUP  BY polls.id 
-HAVING Count(user_responses.*) = Count(questions.*)", self.id]
+    Poll.find_by_sql [
+    " SELECT
+        polls.* 
+      FROM
+        polls 
+      JOIN
+        questions ON questions.poll_id = polls.id 
+      LEFT JOIN (
+        SELECT  
+          responses.user_id, 
+          answer_choices.question_id AS question_id
+        FROM
+          responses 
+        JOIN
+          answer_choices ON answer_choices.id = responses.answer_choice_id 
+        WHERE
+          responses.user_id = ?
+      ) AS user_responses ON user_responses.question_id = questions.id 
+      GROUP BY
+        polls.id 
+      HAVING
+        Count(user_responses.*) = Count(questions.*)
+    ", self.id]
+  end
+
+  def completed_polls
+    Poll
+      .all
+      .left_joins(questions: [ {answer_choices: :responses}] )
+      .where('responses.user_id = ?', self.id)
+      .group(:id)
+      .having("Count(responses.*) = Count(questions.*)")
+      # .left_joins(["SELECT  
+      #     responses.user_id, 
+      #     answer_choices.question_id AS question_id
+      #   FROM
+      #     responses 
+      #   JOIN
+      #     answer_choices ON answer_choices.id = responses.answer_choice_id 
+      #   WHERE
+      #     responses.user_id = ?", self.id])
   end
 
   has_many :authored_polls,
