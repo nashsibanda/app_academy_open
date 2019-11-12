e0c5f5b2b89c3220f6d57ad2e6a430ced93e1ba4
Nash <nashx90@gmail.com>
2019-11-12 13:45:50 +0900

    Complete Searchable module, tests passing

--- SQL/Active Record Under-The-Hood/Build Your Own Active Record/lib/02_searchable.rb
+++ SQL/Active Record Under-The-Hood/Build Your Own Active Record/lib/02_searchable.rb
@@ -3,10 +3,21 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    where_arr = params.keys.map { |col| "#{col} = ?" }
+    where_line = where_arr.join(" AND ")
+    rows = DBConnection.execute(<<-SQL, params.values)
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+      WHERE
+        #{where_line}
+    SQL
+    self.parse_all(rows)
   end
 end
 
 class SQLObject
   # Mixin Searchable here...
+  extend Searchable
 end
