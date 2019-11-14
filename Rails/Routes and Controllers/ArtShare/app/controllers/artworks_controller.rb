f2ba842b05f7e4dbd5d4c14593363fe918c51312
Nash <nashx90@gmail.com>
2019-11-15 00:45:07 +0900

    Add ArtworksController, all REST actions

---
+++ Rails/Routes and Controllers/ArtShare/app/controllers/artworks_controller.rb
@@ -0,0 +1,50 @@
+class ArtworksController < ApplicationController
+  before_action
+    rescue_from ActiveRecord::RecordNotFound, with: :handle_record_not_found
+
+  def index
+    render json: Artwork.all
+  end
+
+  def show
+    artwork = Artwork.find(params[:id])
+    if artwork
+      render json: artwork
+    else
+      render json: artwork.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def create
+    artwork = Artwork.new(artwork_params)
+    if artwork.save
+      render json: artwork
+    else
+      render json: artwork.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+  
+  def update
+    artwork = Artwork.find(params[:id])
+    if artwork.update(artwork_params)
+      render json: artwork
+    else
+      render json: artwork.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    artwork = Artwork.find(params[:id])
+    artwork.destroy
+    render json: artwork
+  end
+
+  private
+  def handle_record_not_found
+    render plain: "Artwork not found", status: :unprocessable_entity
+  end
+
+  def artwork_params
+    params.require(:artwork).permit(:title, :image_url, :artist_id)
+  end
+end
