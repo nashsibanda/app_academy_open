3fa9066449f91cb4df4f9d1bf7aa459726408614
Nash <nashx90@gmail.com>
2019-11-14 22:02:06 +0900

    Add #update, #destroy, refactor to #user_params

--- Rails/Routes and Controllers/RoutesAndControllers/app/controllers/users_controller.rb
+++ Rails/Routes and Controllers/RoutesAndControllers/app/controllers/users_controller.rb
@@ -14,12 +14,37 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(params.require(:user).permit(:name, :email))
+    user = User.new(user_params)
     if user.save
       render json: user
     else
       render json: user.errors.full_messages, status: :unprocessable_entity
     end
+  end
+
+  def update
+    user = User.find(params[:id])
+    if user.update_attributes(user_params)
+      render json: user
+    else
+      render json: user.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    if user
+      User.destroy(params[:id])
+      render json: User.all
+    else
+      render json: user.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 
 end
