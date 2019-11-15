7dd07165c0fbc3a2cd69dcd8fc09ce6a32d06f4b
Nash <nashx90@gmail.com>
2019-11-15 11:29:12 +0900

    Add comments routes and controller, with params in #index

--- Rails/Routes and Controllers/ArtShare/app/controllers/comments_controller.rb
+++ Rails/Routes and Controllers/ArtShare/app/controllers/comments_controller.rb
@@ -1,3 +1,40 @@
 class CommentsController < ApplicationController
+  before_action
+    rescue_from ActiveRecord::RecordNotFound, with: :handle_record_not_found
   
+  def index
+    if params[:artwork_id]
+      comments = Comment.where(artwork_id: params[:artwork_id])
+    elsif params[:user_id]
+      comments = Comment.where(commenter_id: params[:user_id])
+    else
+      comments = Comment.all
+    end
+    render json: comments
+  end
+
+  def create
+    comment = Comment.new(comment_params)
+    if comment.save
+      render json: comment
+    else
+      render json: comment.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    comment = Comment.find(params[:id])
+    comment.destroy
+    render json: comment
+  end
+
+  private
+
+  def handle_record_not_found
+    render plain: "Comment not found", status: :unprocessable_entity
+  end
+
+  def comment_params
+    params.require(:comment).permit(:artwork_id, :commenter_id, :body)
+  end
 end
--- Rails/Routes and Controllers/ArtShare/config/routes.rb
+++ Rails/Routes and Controllers/ArtShare/config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show, :update, :create, :destroy]
   resources :artworks, only: [:show, :update, :create, :destroy]
   resources :artwork_shares, only: [:create, :destroy]
+  resources :comments, only: [:index, :create, :destroy]
 
   resources :users do
     resources :artworks, only: [:index]
