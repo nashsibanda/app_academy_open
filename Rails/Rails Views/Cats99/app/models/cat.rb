b312e4af1ad7201bd70a156ad12d921756040c48
Nash <nashx90@gmail.com>
2019-11-18 14:36:08 +0900

    Fix #age, add #time_since_added_to_cats99

--- Rails/Rails Views/Cats99/app/models/cat.rb
+++ Rails/Rails Views/Cats99/app/models/cat.rb
@@ -5,7 +5,11 @@ class Cat < ApplicationRecord
   validates :color, presence: true, inclusion: { in: %w(black brown grey white tabby), message: "%{value} is not a recognised colour" }
 
   def age
-    time_ago_in_words(self.birth_date, only: :years)
+    time_ago_in_words(self.birth_date, highest_measures: 3 )
+  end
+
+  def time_since_added_to_cats99
+    time_ago_in_words(self.created_at, highest_measures: 3 )
   end
 
 end
