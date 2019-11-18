418c6060f4d3d393ef83dac11a98ea28ab2b9e59
Nash <nashx90@gmail.com>
2019-11-18 19:13:25 +0900

    Validate to avoid cat birth dates in the future

--- Rails/Rails Views/Cats99/app/models/cat.rb
+++ Rails/Rails Views/Cats99/app/models/cat.rb
@@ -3,6 +3,7 @@ class Cat < ApplicationRecord
   validates :birth_date, presence: true
   validates :sex, presence: true, inclusion: { in: %w(M F), message: "%{value} is not a valid sex, please choose M or F"}, length: { is: 1 }
   validates :color, presence: true, inclusion: { in: %w(black brown grey white tabby), message: "%{value} is not a recognised colour" }
+  validate :year_not_in_future
 
   def age
     time_ago_in_words(self.birth_date, highest_measures: 3 )
@@ -10,6 +11,12 @@ class Cat < ApplicationRecord
 
   def time_since_added_to_cats99
     time_ago_in_words(self.created_at, highest_measures: 3 )
+  end
+
+  private
+
+  def year_not_in_future
+    errors[:birth_date] << "cannot be in the future" unless !birth_date || birth_date <= Time.now
   end
 
 end
