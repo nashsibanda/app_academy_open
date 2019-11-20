25d6ef64a1eef3067a6c139c810114392b101c43
Nash <nashx90@gmail.com>
2019-11-20 23:02:38 +0900

    Add password creation and validation helpers to User model

--- Rails/Rails Auth/auth_video_demo/app/models/user.rb
+++ Rails/Rails Auth/auth_video_demo/app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
-  validates :username, presence :true, uniqueness: true
-  validates :password_digest, presence :true, uniqueness: true
+  validates :username, presence: true, uniqueness: true
+  validates :password_digest, presence: true, uniqueness: true
+
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
 end
