fc82974490737e2a339fe185824bbf2324120b2e
Nash <nashx90@gmail.com>
2019-11-20 23:51:21 +0900

    Add sessions, store tokens in users

--- Rails/Rails Auth/auth_video_demo/app/controllers/users_controller.rb
+++ Rails/Rails Auth/auth_video_demo/app/controllers/users_controller.rb
@@ -7,10 +7,14 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      render json: @user
+      render :show
     else
       render json: @user.errors.full_messages
     end
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
   end
 
   private
