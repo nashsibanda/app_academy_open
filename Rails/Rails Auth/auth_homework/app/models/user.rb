4f51dea0e802180ac1ae4cc97f91321aaa0bc6cb
Nash <nashx90@gmail.com>
2019-11-21 17:34:12 +0900

    Create users model and table, with validations

---
+++ Rails/Rails Auth/auth_homework/app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  attr_reader :password
+
+  validates :username, presence: true
+  validates :session_token, presence: true
+  validates :password_digest, presence: { message: 'Password can\'t be blank' }
+  validates :password, length: {minimum:6, allow_nil: true}
+  before_validation :ensure_session_token
+
+end
