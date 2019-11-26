4d9edde1cc4f42c62fa17c714579b66a4e3bb67e
Nash <nashx90@gmail.com>
2019-11-26 21:53:27 +0900

    Tidy up forms and login method

--- Rails/Rails Auth/MusicApp/app/controllers/sessions_controller.rb
+++ Rails/Rails Auth/MusicApp/app/controllers/sessions_controller.rb
@@ -5,9 +5,9 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by_credentials(session_params)
+    @user = User.find_by_credentials(params[:user][:email], params[:user][:password])
     log_in_user!(@user)
-    redirect_to user_url(current_user)
+    redirect_to user_url(@user)
   end
 
   def destroy
