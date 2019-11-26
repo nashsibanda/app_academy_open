7f80d3d73aa3e1895c3bb94c3526ec8468b05e92
Nash <nashx90@gmail.com>
2019-11-26 12:06:51 +0900

    Send welcome_email to new registered users

--- Rails/Rails Auth/Helpers, Layouts and Partials/app/mailers/user_mailer.rb
+++ Rails/Rails Auth/Helpers, Layouts and Partials/app/mailers/user_mailer.rb
@@ -3,6 +3,6 @@ class UserMailer < ApplicationMailer
 
   def welcome_email(user)
     @user = user
-    mail(to: user.email, subject: 'Welcome to a/A!')
+    mail(to: user.username, subject: 'Welcome to a/A!')
   end
 end
