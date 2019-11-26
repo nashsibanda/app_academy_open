e502c31c784bd65d3c4be17d775c5e86b69b8e3a
Nash <nashx90@gmail.com>
2019-11-26 13:43:29 +0900

    Add session token methods to user model

--- Rails/Rails Auth/MusicApp/app/models/user.rb
+++ Rails/Rails Auth/MusicApp/app/models/user.rb
@@ -4,13 +4,34 @@ class User < ApplicationRecord
   validates :email, :session_token, :password_digest, presence: true
   validates :password, length: { minimum:6, allow_null: true, message: "Password must be longer than 6 characters" }
   validates :email, uniqueness: true
+  after_initialize :ensure_session_token
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    return nil unless user.is_password?(password)
+    user
+  end
+  
+  def self.generate_session_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def reset_session_token!
+    self.session_token = self.class.generate_session_token
+  end
 
   def password=(password)
+    @password = password
     self.password_digest = BCrypt::Password.create(password)
   end
 
   def is_password?(password)
     BCrypt::Password.new(self.password_digest) == password
+  end
+
+  private
+  def ensure_session_token
+    self.session_token ||= self.class.generate_session_token
   end
 
 end
