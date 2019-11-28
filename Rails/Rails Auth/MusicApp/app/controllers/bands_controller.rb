2182667023a64ad93dd23474a68beaa94eabe395
Nash <nashx90@gmail.com>
2019-11-28 10:58:58 +0900

    Add band edit, new and show views, DRY form

--- Rails/Rails Auth/MusicApp/app/controllers/bands_controller.rb
+++ Rails/Rails Auth/MusicApp/app/controllers/bands_controller.rb
@@ -2,4 +2,31 @@ class BandsController < ApplicationController
   def index
     @bands = Band.all.order(name: :asc)
   end
+
+  def show
+    @band = Band.find_by(id: params[:id])
+  end
+
+  def new
+    @band = Band.new
+  end
+
+  def create
+    @band = Band.new(band_params)
+    if @band.save
+      redirect_to band_url(@band)
+    else
+      redirect_to bands_url
+    end
+  end
+
+  def edit
+    @band = Band.find_by(id: params[:id])
+  end
+
+  private
+
+  def band_params
+    params.require(:band).permit(:name)
+  end
 end
