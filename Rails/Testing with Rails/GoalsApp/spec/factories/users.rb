c8ed462c0866c61a940fcc4203764272717ebe82
Nash <nashx90@gmail.com>
2019-12-05 21:02:16 +0900

    Add session resource and model, spec skeleton

---
+++ Rails/Testing with Rails/GoalsApp/spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :user do
+    email { "MyString" }
+    password_digest { "MyString" }
+    session_token { "MyString" }
+    activation_token { "MyString" }
+    activated { false }
+  end
+end
