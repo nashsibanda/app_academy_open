92a9072ba4c4c069f4ec7c525c7fa7ee4466970d
Nash <nashx90@gmail.com>
2019-12-06 11:11:30 +0900

    Implement users controller functionality, make some views

--- Rails/Testing with Rails/GoalsApp/app/controllers/users_controller.rb
+++ Rails/Testing with Rails/GoalsApp/app/controllers/users_controller.rb
@@ -1,2 +1,42 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "User successfully created!"
+      redirect_to user_url(@user)
+    else
+      flash[:errors] = @user.errors.full_messages
+      redirect_to new_user_url
+    end
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+  end
+
+  def destroy
+    @user = User.find_by(id: params[:id])
+    if @user
+      @user.destroy
+      flash[:notice] = "User #{@user.email} successfully deleted"
+      redirect_to users_url
+    else
+      flash[:errors] = "User not found"
+      redirect_to users_url
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+
 end
--- Rails/Testing with Rails/GoalsApp/spec/controllers/users_controller_spec.rb
+++ Rails/Testing with Rails/GoalsApp/spec/controllers/users_controller_spec.rb
@@ -21,7 +21,7 @@ RSpec.describe UsersController, type: :controller do
    context "with invalid new user" do
      it "renders the user sign-up page with errors" do
       post :create, params: { user: { email: "shortpassuser", password: "123" } }
-      expect(response).to render_template("new")
+      expect(response).to redirect_to new_user_url
       expect(flash[:errors]).to be_present
     end
   end
@@ -29,7 +29,7 @@ RSpec.describe UsersController, type: :controller do
   context "with valid new user" do
     it "renders the user show page for the new user" do
       post :create, params: { user: { email: "validuser", password: "validpass" } }
-      expect(response).to render_template(user_url(User.last))
+      expect(response).to redirect_to user_url(User.last)
      end
    end
    
@@ -39,8 +39,8 @@ RSpec.describe UsersController, type: :controller do
   describe "GET #show" do
     it "renders the user show page" do
       test_user = FactoryBot.create(:user)
-      get :show, id: test_user.id
-      expect(response).to render_template(user_url(test_user))
+      get :show, params: { id: test_user.id }
+      expect(response).to render_template("show")
       expect(response).to have_http_status(200)
     end
   end
@@ -49,16 +49,16 @@ RSpec.describe UsersController, type: :controller do
     context "with an existing user" do
       it "renders the user index page" do
         test_user = FactoryBot.create(:user)
-        delete :destroy, id: test_user.id
-        expect(response).to render_template("index")
-        expect(response).to have_http_status(200)
+        delete :destroy, params: { id: test_user.id }
+        expect(response).to redirect_to users_url
+        expect(flash[:notice]).to be_present
       end
     end
 
     context "with a non-existent user" do
       it "renders the user index page with an error" do
-        delete :destroy, id: 123456789
-        expect(response).to render_template("index")
+        delete :destroy, params: { id: 193848921 }
+        expect(response).to redirect_to users_url
         expect(flash[:errors]).to be_present
       end
     end
