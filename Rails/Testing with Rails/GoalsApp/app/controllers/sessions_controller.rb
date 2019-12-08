2c04f2b743fdf55f05e8764bee4d2d5476b4c728
Nash <nashx90@gmail.com>
2019-12-08 21:41:10 +0900

    Begin building out session controller methods

--- Rails/Testing with Rails/GoalsApp/app/controllers/sessions_controller.rb
+++ Rails/Testing with Rails/GoalsApp/app/controllers/sessions_controller.rb
@@ -1,3 +1,25 @@
 class SessionsController < ApplicationController
   
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by_credentials(params[:user][:email], params[:user][:password])
+    if @user
+      log_in_user!(@user)
+    else
+      
+    end
+  end
+
+  def destroy
+  
+  end
+
+  private
+  def session_params
+    params.require(:user).permit(:email, :password)
+  end
+  
 end
