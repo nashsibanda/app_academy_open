bc892fb0f20e96416494c4a5bfb3f700403c599c
Nash <nashx90@gmail.com>
2019-12-21 15:14:12 +0000

    Complete site, add responsive sticky headers

---
+++ Rails/Rails and CSS/App Academy Times/app/assets/javascripts/components/sticky-sections-nav.js
@@ -0,0 +1,12 @@
+// window.onscroll = function () { stickyScroll() };
+
+// // var sectionsNav = document.getElementById("sections-nav");
+// // var sticky = $("#sections-nav").offset()["top"];
+
+// function stickyScroll() {
+//   if (window.pageYOffset > $("#sections-nav").offset()["top"]) {
+//     $("#sections-nav").addClass('sticky');
+//   } else {
+//     $("#sections-nav").removeClass('sticky');
+//   }
+// }
