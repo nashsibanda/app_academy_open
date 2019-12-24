47dee8d22a21b7df84da99e0203433dde253ade7
Nash <nashx90@gmail.com>
2019-12-24 09:57:16 +0000

    Add user model, with auth methods

---
+++ Rails/Advanced Rails Forms/RedditOnRails/app/models/user.rb
@@ -0,0 +1,35 @@
+class User < ApplicationRecord
+  attr_reader :password
+  validates :name, :session_token, :password_digest, presence: true
+  validates_length_of :password, minimum: 6, message: "music be at least 6 characters", allow_nil: true
+  after_initialize :ensure_session_token
+
+  def self.generate_session_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def self.find_by_credentials(name, password)
+    user = User.find_by(name: name)
+    return user if user && user.is_password?(password)
+    nil
+  end
+
+  def reset_session_token!
+    self.session_token = self.class.generate_session_token
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(@password)
+  end
+
+  def is_password?(input)
+    input == BCrypt::Password.new(self.password_digest)
+  end
+
+  private
+  def ensure_session_token
+    self.session_token ||= self.class.generate_session_token
+  end
+
+end
