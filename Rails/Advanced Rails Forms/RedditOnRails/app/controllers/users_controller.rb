4be7abd1e166f166bb6e75abafe1619883f08973
Nash <nashx90@gmail.com>
2019-12-24 10:53:19 +0000

    Add user controller methods

--- Rails/Advanced Rails Forms/RedditOnRails/app/controllers/users_controller.rb
+++ Rails/Advanced Rails Forms/RedditOnRails/app/controllers/users_controller.rb
@@ -1,2 +1,64 @@
 class UsersController < ApplicationController
+  
+  def index
+    @users = User.all
+  end
+  
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in_user!(@user)
+      flash[:notice] = "User #{@user.name} successfully created and logged in!"
+      redirect_to user_url(@user)
+    else
+      flash[:errors] = @user.errors.full_messages
+      redirect_to new_user_url
+    end
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+  end
+  
+  def edit
+    @user = User.find_by(id: params[:id])
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+    if @user
+      if @user.update(user_params)
+        flash[:notice] = "User information successfully updated"
+        redirect_to user_url(@user)
+      else
+        flash[:errors] = @user.errors.full_messages
+        redirect_to edit_user_url(@user)
+      end
+    else
+      flash[:notice] = "User not found"
+      redirect_to users_url
+    end
+  end
+
+  def destroy
+    @user = User.find_by(id: params[:id])
+    if @user
+      @user.destroy
+      flash[:notice] = "User #{@user.name} successfully deleted."
+      redirect_to users_url
+    else
+      flash[:notice] = "User not found"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password)
+  end
+
 end
