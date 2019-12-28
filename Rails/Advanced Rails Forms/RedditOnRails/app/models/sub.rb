53fd6ea9789d48aa393984ca11874638dfe6322a
Nash <nashx90@gmail.com>
2019-12-28 12:50:00 +0000

    Add alphanumeric validation to sub name, fix post edit form

--- Rails/Advanced Rails Forms/RedditOnRails/app/models/sub.rb
+++ Rails/Advanced Rails Forms/RedditOnRails/app/models/sub.rb
@@ -1,9 +1,17 @@
 class Sub < ApplicationRecord
   validates_uniqueness_of :name, on: :create, message: "is already taken by another sub"
   validates :name, :description, presence: true
+  validate :name_must_be_alphanumeric
 
   has_many :moderator_moderated_subs, dependent: :destroy
   has_many :moderators, through: :moderator_moderated_subs, source: :moderator
   has_many :posts, dependent: :destroy
   
+  def name_must_be_alphanumeric
+    allowed = ("a".."z").to_a + ("A".."Z").to_a + ("0".."9").to_a + ["_"]
+    if self.name.present? && self.name.split.any? {|ch| !allowed.include?(ch)}
+      errors.add(:name, "can only contain alphanumeric characters and underscores")
+    end
+  end
+
 end
