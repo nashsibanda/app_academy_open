19b354a85dda445f4e73fa99071ad0abd9070a4b
Nash <nashx90@gmail.com>
2020-01-03 14:32:32 +0000

    Add controller base, p02 tests passing

--- Rails/Rails Lite/Rails Lite/lib/controller_base.rb
+++ Rails/Rails Lite/Rails Lite/lib/controller_base.rb
@@ -8,20 +8,31 @@ class ControllerBase
 
   # Setup the controller
   def initialize(req, res)
+    @req = req
+    @res = res
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    check_if_rendered
+    res['Location'] = url
+    res.status = 302
+    @already_built_response = true
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    check_if_rendered
+    @res.write(content)
+    @res['Content-Type'] = content_type
+    @already_built_response = true
   end
 
   # use ERB and binding to evaluate templates
@@ -35,6 +46,11 @@ class ControllerBase
 
   # use this with the router to call action_name (:index, :show, :create...)
   def invoke_action(name)
+  end
+
+  private
+  def check_if_rendered
+    raise Exception if self.already_built_response?
   end
 end
 
