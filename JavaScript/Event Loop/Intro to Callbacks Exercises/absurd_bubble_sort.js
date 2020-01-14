e5479f647f81abcec45e91414af41e2ad6d581d4
Nash <nashx90@gmail.com>
2020-01-15 05:22:53 +0900

    Allow single pass of bubble sort with callback

--- JavaScript/Event Loop/Intro to Callbacks Exercises/absurd_bubble_sort.js
+++ JavaScript/Event Loop/Intro to Callbacks Exercises/absurd_bubble_sort.js
@@ -5,6 +5,41 @@ const reader = readline.createInterface({
   output: process.stdout
 })
 
+function askIfGreaterThan(el1, el2, callback) {
+  reader.question(
+    `Is el1: ${el1} greater than el2: ${el2}? Please enter yes or no: `,
+    function (response) {
+      let greaterThan = (response === "yes" ? true : false);
+      callback(greaterThan);
+  });
+};
+
+function innerBubbleSortLoop(arr, i, madeAnySwaps, outerBubbleSortLoop) {
+  console.log(`In inner bubble sort. START. Array: ${arr}`)
+  if (i < (arr.length - 1)) {
+    askIfGreaterThan(arr[i], arr[i+1], function(isGreaterThan) {
+      let madeAnySwaps = false;
+      if (isGreaterThan) {
+        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+        madeAnySwaps = true;
+      }
+      innerBubbleSortLoop(arr, (i + 1), madeAnySwaps, outerBubbleSortLoop);
+      console.log(`\n In inner bubble sort. Made any swaps: ${madeAnySwaps}; array: ${arr}`);
+    });
+  } else if (i === (arr.length - 1)) {
+    outerBubbleSortLoop(madeAnySwaps);
+  }
+}
+
+function outerBubbleSortLoop(madeAnySwaps) {
+  console.log(`In outer bubble sort. Made any swaps: ${madeAnySwaps}`);
+  reader.close();
+}
+
+let testArr = [3, 2, 1, 4];
+
+innerBubbleSortLoop(testArr, 0, undefined, outerBubbleSortLoop);
+
 function absurdBubbleSort(arr, sortCompletionCallback) {
   
 
