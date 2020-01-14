875a12cf714f4a89e72a396411417371cd96b310
Nash <nashx90@gmail.com>
2020-01-14 20:07:47 +0900

    Complete add_numbers.js

---
+++ JavaScript/Event Loop/Intro to Callbacks Exercises/add_numbers.js
@@ -0,0 +1,26 @@
+const readline = require("readline");
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
+});
+
+function addNumbers(sumParam, numsLeftParam, completionCallback) {
+  let sum = sumParam;
+  let numsLeft = numsLeftParam;
+  if (numsLeft > 0) {
+    reader.question("Please enter a number:", function (numStr) {
+      let num = Number.parseInt(numStr);
+      sum += num;
+      numsLeft--;
+      console.log(`You entered: ${num}.`);
+      addNumbers(sum, numsLeft, completionCallback);
+    })
+  } else if (numsLeft === 0) {
+    completionCallback(sum);
+    reader.close();
+  }
+}
+
+addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
