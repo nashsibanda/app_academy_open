029bddb090d05b71ec1a30cb7a314b140375d00c
Nash <nashx90@gmail.com>
2020-01-15 16:29:07 +0900

    Add won() function, with helpers

--- JavaScript/Event Loop/TicTacToe JS/board.js
+++ JavaScript/Event Loop/TicTacToe JS/board.js
@@ -1,6 +1,7 @@
 class Board {
   constructor() {
-    this.grid = [["", "", ""], ["", "", ""], ["", "", ""]];
+    this.grid = [["X", "", ""], ["", "X", ""], ["", "", "X"]];
+    this.current_player = "X"
   }
 
   print(){
@@ -9,7 +10,51 @@ class Board {
     console.log(this.grid[2]);
   }
 
+  won() {
+    const fullRow = (row) => row.every(el => el === row[0] && el !== "");
+    if (
+      this.grid.some(fullRow)
+      || this.columns()
+      || this.diagonals()
+    ) {
+      return true
+    } else {
+      return false;
+    }
+  }
+
+  columns() {
+    for (let i = 0; i < this.grid.length; i++) {
+      if (
+        this.grid[0][i] === this.grid[1][i]
+        && this.grid[0][i] === this.grid[2][i]
+        && this.grid[0][i] !== ""
+        ) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  diagonals() {
+    if (this.grid[1][1] !== "") {
+      if (
+        this.grid[0][0] === this.grid[1][1]
+        && this.grid[0][0] === this.grid[2][2]
+      ) {
+          return true;
+      } else if (
+          this.grid[0][2] === this.grid[1][1]
+          && this.grid[0][2] === this.grid[2][0]
+      ) {
+        return true
+      }
+    }
+    return false;
+  }
+
 }
 
 const board = new Board;
 board.print();
+console.log(board.won());
