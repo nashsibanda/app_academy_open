f53c2a326fd12e6b3167cc378e85d7b0ffbb47c5
Nash <nashx90@gmail.com>
2020-01-24 15:40:38 +0900

    Add bindEvents(), game working

--- JavaScript/jQuery/jQuery Tic Tac Toe/src/ttt-view.js
+++ JavaScript/jQuery/jQuery Tic Tac Toe/src/ttt-view.js
@@ -3,19 +3,46 @@ class View {
     this.game = game;
     this.$el = $el;
     this.setupBoard();
+    this.bindEvents();
   }
 
-  bindEvents() {}
+  bindEvents() {
+    const that = this;
+    this.$el.on("click", "li", function () {
+      const $square = $(this);
+      const pos = $square.data("pos");
+      console.log(pos)
+      let squareClass = "marked marked-X";
+      let squareMark = "X"
+      if (that.game.currentPlayer === "o") {
+        squareClass = "marked marked-O"
+        squareMark = "O"
+      }
+      try {
+        console.log(that.game.board)
+        that.game.playMove(pos);
+      } catch (error) {
+        alert("This is an invalid move!");
+        return
+      }
+      $square.addClass(squareClass);
+      $square.append("<span>" + squareMark + "</span>");
+    })
+  }
 
   makeMove($square) {}
 
   setupBoard() {
     const $ul = $("<ul></ul>");
     $ul.addClass("grid");
-    for (let i = 0; i < 9; i++) {
-      let $li = $("<li></li>");
-      $li.addClass("grid-square");
-      $ul.append($li);
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        let $li = $("<li></li>");
+        let pos = `[${i}, ${j}]`;
+        $li.addClass("grid-square");
+        $li.attr("data-pos", pos);
+        $ul.append($li);
+      }
     }
     this.$el.append($ul);
   }
