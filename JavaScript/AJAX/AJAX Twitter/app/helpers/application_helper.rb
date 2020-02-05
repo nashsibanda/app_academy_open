11eb7df85596963813455b4c13dcb678e2362e33
Nash <nashx90@gmail.com>
2020-02-05 19:14:23 +0900

    Tweak user select remove btn style, make nice_date helper method

--- JavaScript/AJAX/AJAX Twitter/app/helpers/application_helper.rb
+++ JavaScript/AJAX/AJAX Twitter/app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def nice_date(date, type = "num")
+    case type
+    when "text"
+      date.strftime("%l:%M%P on %B %e, %Y")
+    else
+      date.strftime("%l:%M%P on %m/%d/%Y")
+    end
+  end
+
 end
