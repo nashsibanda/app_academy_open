f9e2c64b6e2b71fee44cf561c9490cdbe1fd0ff9
Nash <nashx90@gmail.com>
2020-02-07 14:53:17 +0900

    Complete Vanilla DOM Manipulation

--- JavaScript/JavaScript DOM API/Vanilla DOM Manipulation/script.js
+++ JavaScript/JavaScript DOM API/Vanilla DOM Manipulation/script.js
@@ -18,14 +18,44 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // adding SF places as list items
 
-  // --- your code here!
+  const addPlace = (e) => {
+    e.preventDefault();
+    const placeInputEl = document.querySelector(".favorite-input");
+    const newPlace = placeInputEl.value;
+    placeInputEl.value = "";
 
+    const placeList = document.getElementById("sf-places");
+    const placeLi = document.createElement("li");
+    placeLi.textContent = newPlace;
+    placeList.appendChild(placeLi);
+  }
 
-
+  document.querySelector(".list-container form").addEventListener("submit", addPlace);
   // adding new photos
 
-  // --- your code here!
+  const togglePuppyForm = (e) => {
+    const form = document.querySelector(".photo-form-container");
+    if (form.classList.contains("hidden")) {
+      form.classList.remove("hidden");
+    } else {
+      form.classList.add("hidden");
+    }
+  }
 
+  const addPuppy = (e) => {
+    e.preventDefault();
+    const newPuppyInput = document.querySelector(".photo-url-input");
+    const newPuppyUrl = newPuppyInput.value;
+    newPuppyInput.value = "";
+    
+    const puppyList = document.querySelector(".dog-photos");
+    const newPuppyImg = document.createElement("img");
+    newPuppyImg.setAttribute("src", newPuppyUrl);
+    const newPuppyLi = document.createElement("li");
+    newPuppyLi.appendChild(newPuppyImg);
+    puppyList.appendChild(newPuppyLi);
+  }
 
-
+  document.querySelector(".photo-show-button").addEventListener("click", togglePuppyForm);
+  document.querySelector(".photo-form-container form").addEventListener("submit", addPuppy);
 });
