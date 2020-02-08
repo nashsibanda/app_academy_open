4234626af5ed942807c4ea429d60303c63867a6a
Nash <nashx90@gmail.com>
2020-02-08 15:18:56 +0900

    Add #ajax function with defaults

--- JavaScript/JavaScript DOM API/jQuery Lite/src/index.js
+++ JavaScript/JavaScript DOM API/jQuery Lite/src/index.js
@@ -15,6 +15,42 @@ window.$1 = (argument) => {
   }
 }
 
+window.$1.extend = (...objects) => {
+  const outputObject = {};
+  objects.forEach(object => {
+    const objectKeys = Object.keys(object);
+    objectKeys.forEach(key => {
+      outputObject[key] = object[key];
+    })
+  })
+  return outputObject;
+}
+
+window.$1.ajax = (customOptions) => {
+  const defaults = {
+    url: window.location.href,
+    method: "GET",
+    data: "",
+    contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+    success(data) {
+      console.log("Success!");
+      console.log(data);
+    },
+    error() {
+      console.log("An error occurred!");
+    }
+  }
+
+  const options = window.$1.extend(defaults, customOptions);
+
+  const request = new XMLHttpRequest;
+  request.open(options.method, options.url);
+  request.onload = function () {
+    options.success(JSON.parse(request.response));
+  };
+  request.send(options.data);
+}
+
 const check = () => {
   console.log(document.readyState);
 }
