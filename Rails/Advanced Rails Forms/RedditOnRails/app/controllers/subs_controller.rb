af6d3532853b4a53e1825be32f1495c65d46459b
Nash <nashx90@gmail.com>
2020-02-15 01:27:17 +0900

    Fix #set_sub helper method

--- Rails/Advanced Rails Forms/RedditOnRails/app/controllers/subs_controller.rb
+++ Rails/Advanced Rails Forms/RedditOnRails/app/controllers/subs_controller.rb
@@ -60,7 +60,7 @@ class SubsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_sub
-      @sub = Sub.includes(posts: [:votes]).(params[:id])
+      @sub = Sub.includes(posts: [:votes]).find_by(id: params[:id])
     end
 
     # Only moderators can edit, update or destroy subs
