9612cbaf576f0e92e4649dd679ecfb0b070bf145
Nash <nashx90@gmail.com>
2020-02-15 15:39:47 +0900

    Add basic linting

--- React/React Fundamentals/Widgets/widgets.jsx
+++ React/React Fundamentals/Widgets/widgets.jsx
@@ -1,16 +1,16 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import Clock from "./clock"
+import React from "react";
+import ReactDOM from "react-dom";
+import Clock from "./clock";
 
 function Root(props) {
   return (
     <main className="rootSection">
       <Clock />
     </main>
-  )
+  );
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  const rootEl = document.getElementById("root")
-  ReactDOM.render(<Root />, rootEl)
-})
+  const rootEl = document.getElementById("root");
+  ReactDOM.render(<Root />, rootEl);
+});
