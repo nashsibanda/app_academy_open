eeb11c952c60153baf5cb7f8063e56715ee7f58e
Nash <nashx90@gmail.com>
2020-02-15 17:03:54 +0900

    Move .jsx files to /scripts directory

---
+++ React/React Fundamentals/Widgets/scripts/widgets.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Clock from "./clock";
+import Tabs from "./tabs";
+
+function Root(props) {
+  const tabsContent = [
+    {
+      title: "One",
+      content: "This is the first page."
+    },
+    {
+      title: "Two",
+      content: "Here is the second page."
+    },
+    {
+      title: "Three",
+      content: "And finally, the third page."
+    },
+    {
+      title: "Four",
+      content: "Surprise! A fourth tab!"
+    }
+  ];
+  return (
+    <main className="rootSection">
+      <Clock />
+      <Tabs {...tabsContent} />
+    </main>
+  );
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const rootEl = document.getElementById("root");
+  ReactDOM.render(<Root />, rootEl);
+});
--- React/React Fundamentals/Widgets/webpack.config.js
+++ React/React Fundamentals/Widgets/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./widgets.jsx",
+  entry: "./scripts/widgets.jsx",
   output: {
     path: path.resolve(__dirname),
     filename: "bundle.js"
