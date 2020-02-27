68a69b3af7300d9739f60a2aec92c812f1624095
Nash <nashx90@gmail.com>
2020-02-28 00:06:46 +0900

    Initial commit for Todos

---
+++ React/Redux Fundamentals/Todos/frontend/todo_redux.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+document.addEventListener("DOMContentLoaded", function() {
+  ReactDOM.render(
+    <h1>Hello, it's me, React!</h1>,
+    document.getElementById("content")
+  );
+});
