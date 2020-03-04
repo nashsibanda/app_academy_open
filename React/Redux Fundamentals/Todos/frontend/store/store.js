f986a31c1c8b2fb302228ae2c042424d07ade7e5
Nash <nashx90@gmail.com>
2020-03-04 23:07:59 +0900

    Add local storage for app state

--- React/Redux Fundamentals/Todos/frontend/store/store.js
+++ React/Redux Fundamentals/Todos/frontend/store/store.js
@@ -2,9 +2,24 @@ import { createStore } from "redux";
 // import { receiveTodo, receiveTodos } from "./../actions/todo_actions";
 import rootReducer from "./../reducers/root_reducer";
 
+const myStorage = window.localStorage;
+
+const initialState = () => {
+  if (myStorage.getItem("localState")) {
+    return JSON.parse(myStorage.getItem("localState"));
+  } else {
+    return undefined;
+  }
+};
+
 const configureStore = createStore(
   rootReducer,
+  initialState(),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+configureStore.subscribe(() => {
+  myStorage.setItem("localState", JSON.stringify(configureStore.getState()));
+});
 
 export default configureStore;
