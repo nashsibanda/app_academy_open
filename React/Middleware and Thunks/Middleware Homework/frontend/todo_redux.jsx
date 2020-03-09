6466fcced049a900b86df9292c5e02dcae47e99a
Nash <nashx90@gmail.com>
2020-03-09 11:42:56 +0900

    Add logging to dispatch, complete phase 1

--- React/Middleware and Thunks/Middleware Homework/frontend/todo_redux.jsx
+++ React/Middleware and Thunks/Middleware Homework/frontend/todo_redux.jsx
@@ -1,14 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import configureStore from './store/store';
+import React from "react";
+import ReactDOM from "react-dom";
+import configureStore from "./store/store";
 
-import Root from './components/root';
+import Root from "./components/root";
 
-document.addEventListener('DOMContentLoaded', () => {
-  const preloadedState = localStorage.state ?
-    JSON.parse(localStorage.state) : {};
+document.addEventListener("DOMContentLoaded", () => {
+  const preloadedState = localStorage.state
+    ? JSON.parse(localStorage.state)
+    : {};
   const store = configureStore(preloadedState);
+  store.dispatch = addLoggingToDispatch(store);
 
-  const root = document.getElementById('content');
+  const root = document.getElementById("content");
   ReactDOM.render(<Root store={store} />, root);
 });
+
+const addLoggingToDispatch = function(store) {
+  const dispatch = store.dispatch;
+  return function(action) {
+    console.log(store.getState());
+    console.log(action);
+    dispatch(action);
+    console.log(store.getState());
+  };
+};
