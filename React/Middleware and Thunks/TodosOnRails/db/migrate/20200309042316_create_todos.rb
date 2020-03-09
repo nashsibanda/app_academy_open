e92ce0903e71860ffab60e12f75efb3c42c34dbb
Nash <nashx90@gmail.com>
2020-03-09 13:42:49 +0900

    Add Todos model, with validations

--- React/Middleware and Thunks/TodosOnRails/db/migrate/20200309042316_create_todos.rb
+++ React/Middleware and Thunks/TodosOnRails/db/migrate/20200309042316_create_todos.rb
@@ -1,6 +1,11 @@
 class CreateTodos < ActiveRecord::Migration[6.0]
   def change
     create_table :todos do |t|
+      t.string :title, null: false
+      t.string :body
+      t.boolean :done, null: false
+      t.date :due
     end
+    add_index :todos, :title
   end
 end
