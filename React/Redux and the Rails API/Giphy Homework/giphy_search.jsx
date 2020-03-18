837ea03f6f0dfe00988ac1996e217109d2f82912
Nash <nashx90@gmail.com>
2020-03-18 14:40:11 +0900

    Add thunk middleware, complete redux cycle

--- React/Redux and the Rails API/Giphy Homework/giphy_search.jsx
+++ React/Redux and the Rails API/Giphy Homework/giphy_search.jsx
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom";
 import configureStore from "./store/store";
 import Root from "./components/root";
 import APIUtil from "./util/api_util";
-import { receiveSearchGiphys } from "./actions/giphy_actions";
+import {
+  receiveSearchGiphys,
+  fetchSearchGiphys
+} from "./actions/giphy_actions";
 
 document.addEventListener("DOMContentLoaded", () => {
   const store = configureStore();
   window.store = store;
   window.APIUtil = APIUtil;
   window.receiveSearchGiphys = receiveSearchGiphys;
+  window.fetchSearchGiphys = fetchSearchGiphys;
 });
