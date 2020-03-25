2938b50bf6200877cade696516f5179878ea6780
Nash <nashx90@gmail.com>
2020-03-25 21:38:33 +0900

    Add HashRouter to entry and root files

--- React/Redux and the Rails API/Pokedex/frontend/pokedex.jsx
+++ React/Redux and the Rails API/Pokedex/frontend/pokedex.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import configureStore from "./store/store";
 import APIUtil from "./util/api_util";
 import Root from "./components/root";
+import { HashRouter, Route } from "react-router-dom";
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
