f20e9d2f06d49bb2bfedfbb34926f7448fafb408
Nash <nashx90@gmail.com>
2020-04-04 04:40:33 +0900

    Bootstrap current user to redux state

--- React/Frontend Auth/BenchBnB/app/javascript/packs/bench_bnb.jsx
+++ React/Frontend Auth/BenchBnB/app/javascript/packs/bench_bnb.jsx
@@ -2,20 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 import configureStore from "../frontend/store/store";
 import Root from "../frontend/components/root";
-import { login } from "../frontend/actions/session_actions";
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
-  const store = configureStore();
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
-  window.testuser = { username: "testnash", password: "testnash" };
-  // window.newuser = {
-  //   username: "testnash4",
-  //   password: "testnash4",
-  //   email: "testnash4",
-  // };
-  window.login = login;
-  // window.signup = signup;
+  let store;
+  if (window.currentUser) {
+    const preloadedState = {
+      entities: {
+        users: { [window.currentUser.id]: window.currentUser },
+      },
+      session: { id: window.currentUser.id },
+    };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
   ReactDOM.render(<Root store={store} />, root);
 });
