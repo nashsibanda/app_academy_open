147ab37be43f823ef73ce70d82ad846847df48a2
Nash <nashx90@gmail.com>
2020-04-03 12:18:11 +0900

    Add session actions, reducer and API utils

--- React/Frontend Auth/Front End Authentication Project/frontend/components/nav_bar/nav_bar.jsx
+++ React/Frontend Auth/Front End Authentication Project/frontend/components/nav_bar/nav_bar.jsx
@@ -1,20 +1,24 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default ({ currentUser, logout }) => {
   const display = (
     <div>
-      <Link className="btn" to="/signup">Sign Up</Link>
-      <Link className="btn" to="/login">Log In</Link>
+      <Link className="btn" to="/signup">
+        Sign Up
+      </Link>
+      <Link className="btn" to="/login">
+        Log In
+      </Link>
     </div>
   );
 
   return (
     <header className="nav-bar">
-      <h1 className="logo">BLUEBIRD</h1>
-      <div>
-        {display}
-      </div>
+      <h1 className="logo">
+        <a href="#">BLUEBIRD</a>
+      </h1>
+      <div>{display}</div>
     </header>
   );
 };
---
+++ React/Frontend Auth/Front End Authentication Project/frontend/utils/session.js
@@ -0,0 +1,22 @@
+export const postUser = user => {
+  $.ajax({
+    type: "post",
+    url: "/api/users",
+    data: { user }
+  });
+};
+
+export const postSession = user => {
+  $.ajax({
+    type: "post",
+    url: "/api/session",
+    data: { user }
+  });
+};
+
+export const deleteSession = () => {
+  $.ajax({
+    type: "delete",
+    url: "/api/session"
+  });
+};
