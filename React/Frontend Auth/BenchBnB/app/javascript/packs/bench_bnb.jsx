838da409b5b7dfb5282070f4c759dcef847cdbd5
Nash <nashx90@gmail.com>
2020-04-04 02:17:16 +0900

    Add root and app components

--- React/Frontend Auth/BenchBnB/app/javascript/packs/bench_bnb.jsx
+++ React/Frontend Auth/BenchBnB/app/javascript/packs/bench_bnb.jsx
@@ -1,21 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import { login, signup } from "../frontend/util/session_api_util";
 import configureStore from "../frontend/store/store";
-import { login } from "../frontend/actions/session_actions";
+import Root from "../frontend/components/root";
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
   const store = configureStore();
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
-  window.testuser = { username: "testnash", password: "testnash" };
+  // window.getState = store.getState;
+  // window.dispatch = store.dispatch;
+  // window.testuser = { username: "testnash", password: "testnash" };
   // window.newuser = {
   //   username: "testnash4",
   //   password: "testnash4",
   //   email: "testnash4",
   // };
-  window.login = login;
+  // window.login = login;
   // window.signup = signup;
-  ReactDOM.render(<h2>Welcome to BenchBnB</h2>, root);
+  ReactDOM.render(<Root store={store} />, root);
 });
