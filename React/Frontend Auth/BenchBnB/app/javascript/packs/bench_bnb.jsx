c4ce6f76b0abf001692cf833e93f23e1c96b8f7f
Nash <nashx90@gmail.com>
2020-04-04 02:01:53 +0900

    Add session actions, reducer and configureStore

--- React/Frontend Auth/BenchBnB/app/javascript/packs/bench_bnb.jsx
+++ React/Frontend Auth/BenchBnB/app/javascript/packs/bench_bnb.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { login, signup } from "../frontend/util/session_api_util";
+// import { login, signup } from "../frontend/util/session_api_util";
+import configureStore from "../frontend/store/store";
+import { login } from "../frontend/actions/session_actions";
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
+  const store = configureStore();
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
   window.testuser = { username: "testnash", password: "testnash" };
-  window.newuser = {
-    username: "testnash4",
-    password: "testnash4",
-    email: "testnash4",
-  };
+  // window.newuser = {
+  //   username: "testnash4",
+  //   password: "testnash4",
+  //   email: "testnash4",
+  // };
   window.login = login;
-  window.signup = signup;
+  // window.signup = signup;
   ReactDOM.render(<h2>Welcome to BenchBnB</h2>, root);
 });
