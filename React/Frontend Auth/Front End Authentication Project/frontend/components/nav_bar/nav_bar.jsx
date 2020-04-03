5bc4cc69d9aafb569e559a90ace2eaa2d32c7e25
Nash <nashx90@gmail.com>
2020-04-03 15:23:46 +0900

    Add bootstrap for current_user in session

--- React/Frontend Auth/Front End Authentication Project/frontend/components/nav_bar/nav_bar.jsx
+++ React/Frontend Auth/Front End Authentication Project/frontend/components/nav_bar/nav_bar.jsx
@@ -2,16 +2,21 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default ({ currentUser, logout }) => {
-  const display = (
+  const display = currentUser ? (
     <div>
-      <Link className="btn" to="/signup">
-        Sign Up
-      </Link>
-      <Link className="btn" to="/login">
-        Log In
-      </Link>
+      <p>Hello, {currentUser.username}</p>
+      <button onClick={logout}>Logout</button>
     </div>
-  );
+  ) : (
+      <div>
+        <Link className="btn" to="/signup">
+          Sign Up
+      </Link>
+        <Link className="btn" to="/login">
+          Log In
+      </Link>
+      </div>
+    );
 
   return (
     <header className="nav-bar">
