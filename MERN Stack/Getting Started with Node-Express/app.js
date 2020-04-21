57eeb7754683890fe091714b3d4de509896cc95c
Nash <nashx90@gmail.com>
2020-04-21 14:52:21 +0900

    Add test routes for users and tweets

--- MERN Stack/Getting Started with Node-Express/app.js
+++ MERN Stack/Getting Started with Node-Express/app.js
@@ -2,13 +2,21 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const db = require("./config/keys").mongoURI;
+const bodyParser = require("body-parser");
+
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.get("/", (req, res) => res.send("Hello New, Wide World"));
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 5000;
 
