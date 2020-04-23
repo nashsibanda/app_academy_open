3248f39e3bfcc05524d24d96410785e8f7871d93
Nash <nashx90@gmail.com>
2020-04-23 20:00:06 +0900

    Use env keys in production

--- MERN Stack/Getting Started with Node-Express/app.js
+++ MERN Stack/Getting Started with Node-Express/app.js
@@ -4,6 +4,7 @@ const app = express();
 const db = require("./config/keys").mongoURI;
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require("path");
 
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
@@ -12,6 +13,13 @@ mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
