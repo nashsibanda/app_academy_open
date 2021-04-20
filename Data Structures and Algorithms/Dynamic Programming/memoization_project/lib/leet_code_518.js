b14b8a366cbc767ee07e7e3f810aab29cdc55fa8
Nash Sibanda <contact@nashsibanda.com>
2021-04-20 21:11:15 +0100

    Finish memoization

--- Data Structures and Algorithms/Dynamic Programming/memoization_project/lib/leet_code_518.js
+++ Data Structures and Algorithms/Dynamic Programming/memoization_project/lib/leet_code_518.js
@@ -1,3 +1,20 @@
 // Work through this problem on https://leetcode.com/problems/coin-change-2/ and use the specs given there.
 // Feel free to use this file for scratch work.
 
+// Solution from a/A course
+function change(amount, coins, memo={}) {
+  let key = amount + '-' + coins;
+  if (key in memo) return memo[key];
+  if (amount === 0) return 1;
+  
+  let currentCoin = coins[coins.length - 1];
+  let totalWays = 0;
+  for (let qty = 0; qty * currentCoin <= amount; qty++) {
+    totalWays += change(amount - qty * currentCoin, coins.slice(0, -1), memo);
+  }
+
+  memo[key] = totalWays;
+  return memo[key];
+}
+
+console.log(change(500, [3,5,7,8,9,10,11])) 
