775aaa1da4a756f45b683488de396fccbe576512
Nash Sibanda <contact@nashsibanda.com>
2021-04-21 14:16:01 +0100

    Solve leetcode 70

--- Data Structures and Algorithms/Dynamic Programming/tabulation_project/lib/leet_code_70.js
+++ Data Structures and Algorithms/Dynamic Programming/tabulation_project/lib/leet_code_70.js
@@ -2,5 +2,17 @@
 // Feel free to use this file for scratch work.
 
 function climbStairs(n) {
+  let table = Array(n + 1);
+  table[0] = 1;
+  table[1] = 1;
 
+  for (let i = 2; i <= n; i++) {
+    table[i] = table[i - 1] + table[i - 2];
+  }
+
+  return table[n];
 }
+
+console.log(climbStairs(2))
+console.log(climbStairs(3))
+console.log(climbStairs(4))
