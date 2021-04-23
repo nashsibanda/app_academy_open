89dc597df67971741367058e24a784042cba85c3
Nash Sibanda <contact@nashsibanda.com>
2021-04-23 17:27:53 +0100

    Complete quicksort

--- Data Structures and Algorithms/Efficient Sorting Algorithms/quick_sort_project/lib/quick_sort.js
+++ Data Structures and Algorithms/Efficient Sorting Algorithms/quick_sort_project/lib/quick_sort.js
@@ -1,5 +1,16 @@
 function quickSort(array) {
+  if (array.length < 2) {
+    return array;
+  }
 
+  const pivot = array.shift();
+  const leftArr = array.filter(el => el < pivot);
+  const rightArr = array.filter(el => el >= pivot);
+
+  const sortedLeft = quickSort(leftArr);
+  const sortedRight = quickSort(rightArr);
+
+  return [...sortedLeft, pivot, ...sortedRight];
 }
 
 
