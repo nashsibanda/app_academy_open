e080c788ac6679a895051544c262c3b911bf3a97
Nash Sibanda <contact@nashsibanda.com>
2021-04-23 17:24:32 +0100

    Complete merge sort, initial commit for quicksort

--- Data Structures and Algorithms/Efficient Sorting Algorithms/merge_sort_project/lib/merge_sort.js
+++ Data Structures and Algorithms/Efficient Sorting Algorithms/merge_sort_project/lib/merge_sort.js
@@ -1,9 +1,31 @@
 function merge(array1, array2) {
+  const output = [];
+  while (array1.length || array2.length) {
+    const left = array1.length ? array1[0] : Infinity;
+    const right = array2.length ? array2[0] : Infinity;
 
+    if (left > right) {
+      output.push(array2.shift());
+    } else {
+      output.push(array1.shift());
+    }
+  }
+  return output;
 }
 
 function mergeSort(array) {
+  if (array.length < 2) {
+    return array;
+  }
 
+  const midPoint = Math.floor(array.length / 2);
+  const leftArr = array.slice(0, midPoint);
+  const rightArr = array.slice(midPoint);
+
+  const sortedLeft = mergeSort(leftArr);
+  const sortedRight = mergeSort(rightArr);
+
+  return merge(sortedLeft, sortedRight);
 }
 
 module.exports = {
