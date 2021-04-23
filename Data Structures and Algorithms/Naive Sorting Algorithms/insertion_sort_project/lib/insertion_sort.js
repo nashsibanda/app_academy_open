cad3872d17c9673daa62373febf45c559dbbd4b0
Nash Sibanda <contact@nashsibanda.com>
2021-04-23 16:57:18 +0100

    Complete insertion sort

--- Data Structures and Algorithms/Naive Sorting Algorithms/insertion_sort_project/lib/insertion_sort.js
+++ Data Structures and Algorithms/Naive Sorting Algorithms/insertion_sort_project/lib/insertion_sort.js
@@ -1,7 +1,13 @@
 function insertionSort(arr) {
-
+  for (let i = 0; i < arr.length; i++) {
+    const currentElement = arr[i];
+    for (var j = i - 1; j >= 0 && currentElement < arr[j]; j--) {
+      arr[j + 1] = arr[j];
+    }
+    arr[j + 1] = currentElement;
+  }
 }
 
 module.exports = {
-    insertionSort
+  insertionSort,
 };
